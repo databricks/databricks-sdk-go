baad9e33a33ecd481632e16b45c75116a628d1fe
Serge Smertin <serge.smertin@databricks.com>
2022-08-24 19:01:41 +0200

    Generate libraries service

--- workspaces/client.go
+++ workspaces/client.go
@@ -5,8 +5,13 @@ import (
 	"github.com/databricks/databricks-sdk-go/databricks/client"
 	"github.com/databricks/databricks-sdk-go/service/clusters"
 	"github.com/databricks/databricks-sdk-go/service/commands"
+	"github.com/databricks/databricks-sdk-go/service/dbfs"
+	"github.com/databricks/databricks-sdk-go/service/deltapipelines"
 	"github.com/databricks/databricks-sdk-go/service/groups"
+	"github.com/databricks/databricks-sdk-go/service/instancepools"
+	"github.com/databricks/databricks-sdk-go/service/instanceprofiles"
 	"github.com/databricks/databricks-sdk-go/service/jobs"
+	"github.com/databricks/databricks-sdk-go/service/libraries"
 	"github.com/databricks/databricks-sdk-go/service/serviceprincipals"
 	"github.com/databricks/databricks-sdk-go/service/users"
 )
@@ -14,10 +19,15 @@ import (
 type WorkspacesClient struct {
 	Config *databricks.Config
 
-	Clusters          *clusters.ClustersAPI
+	Clusters          clusters.ClustersService
 	Commands          *commands.CommandsAPI
+	Dbfs              dbfs.DbfsService
+	DeltaPipelines    deltapipelines.DeltapipelinesService
 	Groups            groups.GroupsService
 	Jobs              jobs.JobsService
+	InstancePools     instancepools.InstancepoolsService
+	InstanceProfiles  instanceprofiles.InstanceprofilesService
+	Libraries         libraries.LibrariesService
 	ServicePrincipals serviceprincipals.ServiceprincipalsService
 	Users             users.UsersService
 }
@@ -35,10 +45,15 @@ func New(c ...*databricks.Config) *WorkspacesClient {
 	apiClient := client.New(cfg)
 	return &WorkspacesClient{
 		Config:            cfg,
-		Clusters:          clusters.New(cfg),
+		Clusters:          clusters.New(apiClient),
 		Commands:          commands.New(cfg),
+		Dbfs:              dbfs.New(apiClient),
+		DeltaPipelines:    deltapipelines.New(apiClient),
 		Groups:            groups.New(apiClient),
 		Jobs:              jobs.New(apiClient),
+		InstancePools:     instancepools.New(apiClient),
+		InstanceProfiles:  instanceprofiles.New(apiClient),
+		Libraries:         libraries.New(apiClient),
 		ServicePrincipals: serviceprincipals.New(apiClient),
 		Users:             users.New(apiClient),
 	}
