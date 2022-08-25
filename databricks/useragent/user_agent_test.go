9b87d473505d460640aae75f67f4bb9f82971308
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-08-25 16:29:35 +0200

    Fix user agent tests on go1.19 (#7)

--- databricks/useragent/user_agent_test.go
+++ databricks/useragent/user_agent_test.go
@@ -11,12 +11,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGoVersion(t *testing.T) {
+	// The function `goVersion` returns a semver canonicalized Go version,
+	// but `runtime.Version` may or may not include a patch version.
+	// Correct for that in the test below.
+	parts := strings.SplitN(strings.TrimPrefix(runtime.Version(), "go"), ".", 3)
+	if len(parts) == 2 {
+		parts = append(parts, "0")
+	}
+	assert.Equal(t, goVersion(), strings.Join(parts, "."))
+}
+
 func TestFromContext_Default(t *testing.T) {
 	userAgent := FromContext(context.Background())
 	// tests may be developed and run on different versions of different things
 	expectedFormat := "unknown/0.0.0 databricks-sdk-go/%s go/%s os/%s"
-	expected := fmt.Sprintf(expectedFormat, internal.Version,
-		strings.TrimPrefix(runtime.Version(), "go"), runtime.GOOS)
+	expected := fmt.Sprintf(expectedFormat, internal.Version, goVersion(), runtime.GOOS)
 	assert.Equal(t, expected, userAgent)
 }
 
@@ -29,7 +39,6 @@ func TestFromContext_Custom(t *testing.T) {
 
 	// tests may be developed and run on different versions of different things
 	expectedFormat := "unit-tests/0.0.1 databricks-sdk-go/%s go/%s os/%s pulumi/3.8.4 a/b terraform/1.2.5"
-	expected := fmt.Sprintf(expectedFormat, internal.Version,
-		strings.TrimPrefix(runtime.Version(), "go"), runtime.GOOS)
+	expected := fmt.Sprintf(expectedFormat, internal.Version, goVersion(), runtime.GOOS)
 	assert.Equal(t, expected, userAgent)
 }
