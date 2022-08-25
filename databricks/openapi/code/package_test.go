8e5ac198537ec91559eec6e386f8a961532b377f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-08-25 16:30:00 +0200

    Fix test under openapi/code (#6)

--- databricks/openapi/code/package_test.go
+++ databricks/openapi/code/package_test.go
@@ -15,9 +15,10 @@ func TestCommentFromDescription(t *testing.T) {
 		one common security and governance approach across all of your data 
 		and cloud platforms.`}
 	assert.Equal(t, strings.TrimLeft(`
-    // A data lakehouse unifies the best of data warehouses and data lakes in 
-    // one simple platform to handle all your data, analytics and AI use cases. 
-    // It's built on an open and reliable data foundation that efficiently 
-    // handles all data types and applies one common security and governance `, 
-	"\n"), n.Comment("    // ", 80))
+    // A data lakehouse unifies the best of data warehouses and data lakes in
+    // one simple platform to handle all your data, analytics and AI use cases.
+    // It's built on an open and reliable data foundation that efficiently
+    // handles all data types and applies one common security and governance
+    // approach across all of your data and cloud platforms.`,
+		"\n\t "), n.Comment("    // ", 80))
 }
