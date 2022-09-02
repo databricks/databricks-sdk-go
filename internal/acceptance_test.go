b9b7e3ea16fc8b7bd8f6c5e6216a4cf4ca1b030f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-09-02 16:49:51 +0200

    Make Azure CLI integration test work

--- internal/acceptance_test.go
+++ internal/acceptance_test.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"context"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/databricks/databricks-sdk-go/databricks"
@@ -45,7 +47,51 @@ func TestAccExplicitDatabricksCfg(t *testing.T) {
 	assert.NotEmpty(t, v)
 }
 
+func temporaryDirectory(t *testing.T) string {
+	dir, err := os.MkdirTemp(os.TempDir(), "tmp.*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Remove temporary directory upon test completion.
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+
+	return dir
+}
+
+func setEnv(t *testing.T, key, value string) {
+	cur, ok := os.LookupEnv(key)
+	os.Setenv(key, value)
+
+	// Revert environment variable upon test completion.
+	t.Cleanup(func() {
+		if ok {
+			os.Setenv(key, cur)
+		} else {
+			os.Unsetenv(key)
+		}
+	})
+}
+
 func TestAccExplicitAzureCliAuth(t *testing.T) {
+	setEnv(t, "AZURE_CONFIG_DIR", temporaryDirectory(t))
+
+	// Login with Azure CLI
+	cmd := exec.Command(
+		"az",
+		"login",
+		"--service-principal",
+		"--user", GetEnvOrSkipTest(t, "ARM_CLIENT_ID"),
+		"--password", GetEnvOrSkipTest(t, "ARM_CLIENT_SECRET"),
+		"--tenant", GetEnvOrSkipTest(t, "ARM_TENANT_ID"),
+	)
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("error running az: %s (%s)", err, out)
+	}
+
 	ws := workspaces.New(&databricks.Config{
 		AzureResourceID: GetEnvOrSkipTest(t, "DATABRICKS_AZURE_RESOURCE_ID"),
 		Credentials:     databricks.AzureCliCredentials{},
