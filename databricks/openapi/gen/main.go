40834a0699d217e15d15635a3ea51f76790573f0
Serge Smertin <serge.smertin@databricks.com>
2022-09-03 20:55:48 +0200

    intermediate commit

--- databricks/openapi/gen/main.go
+++ databricks/openapi/gen/main.go
@@ -1,14 +1,17 @@
-// Usage: go run databricks/openapi/gen/main.go -spec /path/to/spec.json
+// Usage: go run databricks/openapi/gen/main.go -target service -spec /path/to/spec.json
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/databricks/databricks-sdk-go/databricks/openapi/code"
@@ -18,6 +21,7 @@ var ctx Context
 
 func main() {
 	flag.StringVar(&ctx.Spec, "spec", "", "location of the spec file")
+	flag.StringVar(&ctx.Target, "target", "", "path to directory with .codegen.json")
 	flag.BoolVar(&ctx.DryRun, "dry-run", false, "print to stdout instead of real files")
 
 	flag.Parse()
@@ -37,6 +41,7 @@ func main() {
 
 type Context struct {
 	Spec   string
+	Target string
 	DryRun bool
 }
 
@@ -45,28 +50,45 @@ func (c *Context) Generate() error {
 	if err != nil {
 		return err
 	}
-	return (&Render{
-		ctx:   c,
-		batch: batch,
-		tmpl:  template.Must(template.ParseGlob("databricks/openapi/gen/*.tmpl")),
-		fileset: map[string]string{
-			"api.go.tmpl":       "service/{{.Name}}/api.go",
-			"interface.go.tmpl": "service/{{.Name}}/interface.go",
-			"model.go.tmpl":     "service/{{.Name}}/model.go",
-		},
-	}).Run()
+	f, err := os.Open(fmt.Sprintf("%s/.codegen.json", c.Target))
+	if err != nil {
+		return fmt.Errorf("no .codegen.json file in %s: %w", c.Target, err)
+	}
+	defer f.Close()
+	raw, err := io.ReadAll(f)
+	if err != nil {
+		return fmt.Errorf("read all: %w", err)
+	}
+	var render Render
+	err = json.Unmarshal(raw, &render)
+	if err != nil {
+		return fmt.Errorf(".codegen.json: %w", err)
+	}
+	render.ctx = c
+	render.batch = batch
+	var tmpls []string
+	fileset := map[string]string{}
+	for filename, v := range render.Fileset {
+		filename = filepath.Join(c.Target, filename)
+		tmpls = append(tmpls, filename)
+		fileset[filepath.Base(filename)] = v
+	}
+	render.Fileset = fileset
+	render.tmpl = template.Must(template.ParseFiles(tmpls...))
+	return render.Run()
 }
 
 type Render struct {
-	ctx     *Context
-	tmpl    *template.Template
-	batch   *code.Batch
-	fileset map[string]string
+	ctx       *Context
+	tmpl      *template.Template
+	batch     *code.Batch
+	Formatter string            `json:"formatter"`
+	Fileset   map[string]string `json:"fileset"`
 }
 
 func (r *Render) Run() error {
 	for _, pkg := range r.batch.Packages {
-		for k, v := range r.fileset {
+		for k, v := range r.Fileset {
 			err := r.File(pkg, k, v)
 			if err != nil {
 				return fmt.Errorf("%s: %w", pkg.Name, err)
@@ -81,8 +103,8 @@ func (r *Render) File(pkg *code.Package, contentTRef, nameT string) error {
 	if err != nil {
 		return fmt.Errorf("parse %s: %w", nameT, err)
 	}
-	var filename, contents strings.Builder
-	err = nt.Execute(&filename, pkg)
+	var childFilename, contents strings.Builder
+	err = nt.Execute(&childFilename, pkg)
 	if err != nil {
 		return fmt.Errorf("exec %s: %w", nameT, err)
 	}
@@ -91,7 +113,7 @@ func (r *Render) File(pkg *code.Package, contentTRef, nameT string) error {
 		return fmt.Errorf("exec %s: %w", contentTRef, err)
 	}
 	if r.ctx.DryRun {
-		fmt.Printf("\n---\nDRY RUN: %s\n---\n%s", &filename, &contents)
+		fmt.Printf("\n---\nDRY RUN: %s\n---\n%s", &childFilename, &contents)
 		return nil
 	}
 	if nameT == "stdout" {
@@ -99,20 +121,21 @@ func (r *Render) File(pkg *code.Package, contentTRef, nameT string) error {
 		println(contents.String())
 		return nil
 	}
-	_, err = os.Stat(filename.String())
+	targetFilename := filepath.Join(r.ctx.Target, childFilename.String())
+	_, err = os.Stat(targetFilename)
 	if errors.Is(err, fs.ErrNotExist) {
-		err = os.MkdirAll(path.Dir(filename.String()), 0o755)
+		err = os.MkdirAll(path.Dir(targetFilename), 0o755)
 		if err != nil {
 			return fmt.Errorf("cannot create parent folders: %w", err)
 		}
 	}
-	file, err := os.OpenFile(filename.String(), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
+	file, err := os.OpenFile(targetFilename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
 	if err != nil {
-		return fmt.Errorf("open %s: %w", &filename, err)
+		return fmt.Errorf("open %s: %w", targetFilename, err)
 	}
 	_, err = file.WriteString(contents.String())
 	if err != nil {
-		return fmt.Errorf("write %s: %w", &filename, err)
+		return fmt.Errorf("write %s: %w", targetFilename, err)
 	}
 	return file.Close()
 }
