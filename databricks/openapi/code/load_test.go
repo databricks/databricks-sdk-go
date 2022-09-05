57cd60629ce557539c25a923ea73c832a60976f1
Serge Smertin <serge.smertin@databricks.com>
2022-09-05 20:13:06 +0200

    make tests run again

--- databricks/openapi/code/load_test.go
+++ databricks/openapi/code/load_test.go
@@ -6,25 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCombined(t *testing.T) {
-	batch, err := NewFromFile("/private/tmp/processed-databricks-workspace-all.json")
-	assert.NoError(t, err)
-
-	for k, v := range batch.Packages["clusters"].services["Clusters"].methods {
-		t.Logf("%s: %v", k, v.Shortcut())
-	}
-
-	// s := batch.Packages["permissions"].services["Permissions"].methods["GetObjectPermissions"].Shortcut()
-	s := batch.Packages["clusters"].services["Clusters"].methods["create"].Shortcut()
-	t.Logf("%v", s)
-
-	w := batch.Packages["clusters"].services["Clusters"].methods["create"].Wait()
-	b := w.Bind()
-	t.Logf("%v", b)
-
-	assert.Len(t, batch.Packages, 1)
-}
-
 func TestBasic(t *testing.T) {
 	batch, err := NewFromFile("../testdata/spec.json")
 	assert.NoError(t, err)
--- internal/init_test.go
+++ internal/init_test.go
@@ -1,19 +1,13 @@
 package internal
 
 import (
-	"context"
 	"fmt"
 	"math/rand"
 	"os"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/databricks/databricks-sdk-go/databricks"
-	"github.com/databricks/databricks-sdk-go/databricks/client"
-	"github.com/databricks/databricks-sdk-go/retries"
-	"github.com/databricks/databricks-sdk-go/service/clusters"
-	"github.com/stretchr/testify/assert"
 )
 
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -48,30 +42,4 @@ func RandomName(prefix ...string) string {
 		return fmt.Sprintf("%s%s", strings.Join(prefix, ""), b)
 	}
 	return string(b)
-}
-
-func startDefaultTestCluster(t *testing.T, ctx context.Context, apiClient *client.DatabricksClient) {
-	clusterService := clusters.NewClusters(apiClient)
-	clusterId := GetEnvOrSkipTest(t, "TEST_DEFAULT_CLUSTER_ID")
-	err := retries.Wait(ctx, 10*time.Minute, func() *retries.Err {
-		clusterDetails, err := clusterService.Get(ctx, clusters.GetRequest{
-			ClusterId: clusterId,
-		})
-		if err != nil {
-			return retries.Halt(err)
-		}
-		if clusterDetails.State == clusters.ClusterInfoStateRunning {
-			return nil
-		}
-		if clusterDetails.State == clusters.ClusterInfoStateTerminated {
-			err = clusterService.Start(ctx, clusters.StartCluster{
-				ClusterId: clusterId,
-			})
-			if err != nil {
-				return retries.Halt(err)
-			}
-		}
-		return retries.Continue(fmt.Errorf("%s", clusterDetails.State))
-	})
-	assert.NoError(t, err)
 }
