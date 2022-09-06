b1f40a0128b379f6168e3be6df34ede5a9edfe09
Shreyas Goenka <shreyas.goenka@databricks.com>
2022-09-06 11:04:16 +0200

    cleaned up and made the test more robust

--- internal/dbfs_test.go
+++ internal/dbfs_test.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"context"
 	"encoding/base64"
+	"path/filepath"
 	"testing"
 
 	"github.com/databricks/databricks-sdk-go/service/dbfs"
@@ -15,19 +16,45 @@ func TestAccListDbfsIntegration(t *testing.T) {
 	t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
 	ctx := context.Background()
 	wsc := workspaces.New()
-	dbfsTestFilePath := RandomName("/test-file-")
-	dbfsTestDirPath := RandomName("/tmp/databricks-go-sdk/integration/dbfs/TestDir-")
+	testFile1 := RandomName("test-file-1-")
+	testFile2 := RandomName("test-file-2-")
+	dbfsTestDirPath1 := RandomName("/tmp/databricks-go-sdk/integration/dbfs/TestDir1-")
+	dbfsTestDirPath2 := RandomName("/tmp/databricks-go-sdk/integration/dbfs/TestDir2-")
 
-	// create file
+	t.Cleanup(func() {
+		// recursively delete the test dir1 and the test files inside it
+		err := wsc.Dbfs.Delete(ctx, dbfs.DeleteRequest{
+			Path:      dbfsTestDirPath1,
+			Recursive: true,
+		},
+		)
+		require.NoError(t, err)
+		// recursively delete the test dir2 and the test files inside it
+		err = wsc.Dbfs.Delete(ctx, dbfs.DeleteRequest{
+			Path:      dbfsTestDirPath2,
+			Recursive: true,
+		},
+		)
+		require.NoError(t, err)
+	})
+
+	// make test dir2 in workspace root
+	err := wsc.Dbfs.MkDirs(ctx, dbfs.MkDirsRequest{
+		Path: dbfsTestDirPath2,
+	},
+	)
+	require.NoError(t, err)
+
+	// create testFile1 in test dir2
 	createdFile, err := wsc.Dbfs.Create(ctx,
 		dbfs.CreateRequest{
-			Path:      dbfsTestFilePath,
+			Path:      filepath.Join(dbfsTestDirPath2, testFile1),
 			Overwrite: true,
 		},
 	)
 	require.NoError(t, err)
 
-	// write 'Hello, World!' to file
+	// write 'Hello, World!' to testFile1
 	err = wsc.Dbfs.AddBlock(ctx, dbfs.AddBlockRequest{
 		Data:   base64.StdEncoding.EncodeToString([]byte("Hello, World!")),
 		Handle: createdFile.Handle,
@@ -35,82 +62,72 @@ func TestAccListDbfsIntegration(t *testing.T) {
 	)
 	require.NoError(t, err)
 
-	// Close file handle
+	// Close testFile1 handle
 	err = wsc.Dbfs.Close(ctx, dbfs.CloseRequest{
 		Handle: createdFile.Handle,
 	},
 	)
 	require.NoError(t, err)
 
-	// Get file status
+	// Get testFile1 status
 	testFileStatus, err := wsc.Dbfs.GetStatus(ctx, dbfs.GetStatusRequest{
-		Path: dbfsTestFilePath,
+		Path: filepath.Join(dbfsTestDirPath2, testFile1),
 	},
 	)
 	require.NoError(t, err)
-	assert.True(t, testFileStatus.Path == dbfsTestFilePath)
+	assert.True(t, testFileStatus.Path == filepath.Join(dbfsTestDirPath2, testFile1))
 	assert.True(t, testFileStatus.IsDir == false)
 
-	// List all files in workspace root and assert test file is found
+	// List all files in test dir2 and assert testFile1 is found
 	listOfFilesInWorkspaceRoot, err := wsc.Dbfs.ListStatus(ctx,
 		dbfs.ListStatusRequest{
-			Path: "/",
+			Path: dbfsTestDirPath2,
 		},
 	)
 	require.NoError(t, err)
 	foundTestFile := false
 	for _, fileInfo := range listOfFilesInWorkspaceRoot.Files {
-		if fileInfo.Path == dbfsTestFilePath {
+		if fileInfo.Path == filepath.Join(dbfsTestDirPath2, testFile1) {
 			foundTestFile = true
 		}
 	}
 	assert.True(t, foundTestFile)
 
-	// make test dir in workspace root
+	// make test dir1 in workspace root
 	err = wsc.Dbfs.MkDirs(ctx, dbfs.MkDirsRequest{
-		Path: dbfsTestDirPath,
+		Path: dbfsTestDirPath1,
 	},
 	)
 	require.NoError(t, err)
 
-	t.Cleanup(func() {
-		// recursively delete the test dir and the test files inside it
-		err = wsc.Dbfs.Delete(ctx, dbfs.DeleteRequest{
-			Path:      dbfsTestDirPath,
-			Recursive: true,
-		},
-		)
-		require.NoError(t, err)
-	})
-
-	// move test file to test dir
+	// move testFile1 to test dir1
 	err = wsc.Dbfs.Move(ctx, dbfs.MoveRequest{
-		SourcePath:      dbfsTestFilePath,
-		DestinationPath: dbfsTestDirPath + dbfsTestFilePath,
+		SourcePath:      filepath.Join(dbfsTestDirPath2, testFile1),
+		DestinationPath: filepath.Join(dbfsTestDirPath1, testFile1),
 	},
 	)
 	require.NoError(t, err)
 
-	// put a new file (byebye-world.txt) in test dir
+	// put a new file testFile2 in test dir1
 	err = wsc.Dbfs.Put(ctx, dbfs.PutRequest{
-		Path:      dbfsTestDirPath + "/byebye-world.txt",
+		Path:      filepath.Join(dbfsTestDirPath1, testFile2),
 		Contents:  base64.StdEncoding.EncodeToString([]byte("Bye Bye, World!")),
 		Overwrite: true,
 	},
 	)
 	require.NoError(t, err)
 
-	// assert on contents of original test file
+	// assert on contents of testFile1
 	contentsTestFile, err := wsc.Dbfs.Read(ctx, dbfs.ReadRequest{
-		Path: dbfsTestDirPath + dbfsTestFilePath,
+		Path: filepath.Join(dbfsTestDirPath1, testFile1),
 	},
 	)
 	require.NoError(t, err)
 	assert.True(t, contentsTestFile.Data == base64.StdEncoding.EncodeToString([]byte("Hello, World!")))
 
-	// assert on contents of byebye-world.txt
+	// assert on contents of testFile2
 	contentsByeByeWorldFile, err := wsc.Dbfs.Read(ctx, dbfs.ReadRequest{
-		Path: dbfsTestDirPath + "/byebye-world.txt",
+		Path: filepath.Join(dbfsTestDirPath1, testFile2),
 	},
 	)
 	require.NoError(t, err)
