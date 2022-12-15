85648dcf8e722a648d80017a485da328403f09aa
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-12-15 09:23:26 +0100

    Update doc.go

--- service/dbfs/doc.go
+++ service/dbfs/doc.go
@@ -4,10 +4,14 @@
 // [github.com/databricks/databricks-sdk-go/workspaces.New] to simplify
 // configuration experience.
 //
-// Please use higher-level [DbfsAPI.Open] and [DbfsAPI.Overwrite] to work with
-// remote files via [io.Reader] interface. Internally, these methods wrap
-// the low level [DbfsAPI.Create], [DbfsAPI.Close], [DbfsAPI.Read], and
-// [DbfsAPI.AddBlock]:
+// Please use the high-level [DbfsAPI.Open] and [DbfsAPI.Overwrite] methods
+// to work with remote files through Go's [io] interfaces. The return value
+// of [DbfsAPI.Open] implements the [io.Reader] and [io.WriterTo] interfaces.
+// The [io.WriterTo] interface is used by [io.Copy] and maximizes throughput by
+// reading data with the DBFS maximum read chunk size of 1MB.
+//
+// Internally, these methods wrap the low level [DbfsAPI.Create],
+// [DbfsAPI.Close], [DbfsAPI.Read], and [DbfsAPI.AddBlock] methods:
 //
 //	upload, _ := os.Open("/path/to/local/file.ext")
 //	_ = w.Dbfs.Overwrite(ctx, "/path/to/remote/file", upload)
