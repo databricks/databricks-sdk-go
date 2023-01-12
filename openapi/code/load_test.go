b0e23fe5eabf82a2a907e768f163fd773448b9c1
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-01-12 13:48:43 +0100

    enable format on save

--- openapi/code/load_test.go
+++ openapi/code/load_test.go
@@ -70,7 +70,7 @@ func TestMethodsReport(t *testing.T) {
 	t.SkipNow()
 	home, _ := os.UserHomeDir()
 	batch, err := NewFromFile(filepath.Join(home,
-		"universe/bazel-bin/openapi/all-internal.json"), []string{})
+		"universe/bazel-bin/openapi/all-internal.json"))
 	assert.NoError(t, err)
 
 	for _, pkg := range batch.Packages() {
