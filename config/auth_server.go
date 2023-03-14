66418dd9f0e91c2f47dec5fd7d3071e57cfda755
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-03-14 14:09:48 +0100

    Make auth server more like Azure msi

--- config/auth_server.go
+++ config/auth_server.go
@@ -6,13 +6,47 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/databricks/databricks-sdk-go/logger"
+	"golang.org/x/oauth2"
 )
 
+// timeout to wait for local auth server to respond
+const authServerTimeout = 10 * time.Second
+
 type serverResponse struct {
-	Host    string            `json:"host"`
-	Headers map[string]string `json:"headers"`
+	// Optional URL of the Databricks host to connect to.
+	Host string `json:"host"`
+
+	// The requested access token. When you call a secured REST API,
+	// the token is embedded in the Authorization request header field as
+	// a "bearer" token, allowing the API to authenticate the caller.
+	AccessToken string `json:"access_token"`
+
+	// The type of token, which is a "Bearer" access token, which means the
+	// resource can give access to the bearer of this token.
+	TokenType string `json:"token_type"`
+
+	// The timespan when the access token expires.
+	// The date is represented as the number of seconds from "1970-01-01T0:0:0Z UTC"
+	// If zero the token is valid forever.
+	ExpiresOn json.Number `json:"expires_on"`
 }
 
+// Credentials provider that fetches a token from a locally running HTTP server.
+// The credentials provider will perform a GET request to the configured URL.
+//
+// The header "Metadata: true" will be added to the request, which must be
+// verified by server to prevent SSRF attacks
+//
+// The server is expected to return a JSON response with the following fields:
+//
+//	host: Optional URL of the Databricks host to connect to.
+//	access_token: The requested access token.
+//	token_type: The type of token, which is a "Bearer" access token.
+//	expires_on: The timespan when the access token expires.
 type ServerCredentials struct{}
 
 func (c ServerCredentials) Name() string {
@@ -24,6 +58,16 @@ func (c ServerCredentials) Configure(ctx context.Context, cfg *Config) (func(*ht
 		return nil, nil
 	}
 
+	parsedAuthServerUrl, err := url.Parse(cfg.AuthServerUrl)
+	if err != nil {
+		return nil, fmt.Errorf("invalid auth server URL: %w", err)
+	}
+
+	// only allow localhost URLs
+	if parsedAuthServerUrl.Hostname() != "localhost" && parsedAuthServerUrl.Hostname() != "127.0.0.1" {
+		return nil, fmt.Errorf("invalid auth server URL: %s", cfg.AuthServerUrl)
+	}
+
 	resp, err := makeRequest(cfg.AuthServerUrl)
 	if err != nil {
 		return nil, nil
@@ -32,29 +76,35 @@ func (c ServerCredentials) Configure(ctx context.Context, cfg *Config) (func(*ht
 	if resp == nil {
 		return nil, nil
 	}
+
+	if resp.Host == "" {
+		return nil, nil
+	}
+
+	if cfg.Host != "" && cfg.Host != resp.Host {
+		return nil, nil
+	}
+
 	cfg.Host = resp.Host
 
-	return func(r *http.Request) error {
-		resp, err := makeRequest(cfg.AuthServerUrl)
-		if err != nil {
-			return err
-		}
-		if resp == nil {
-			return fmt.Errorf("no response from auth server")
-		}
-
-		for k, v := range resp.Headers {
-			r.Header.Set(k, v)
-		}
-
-		return nil
-	}, nil
+	ts := authServerTokenSource(cfg.AuthServerUrl)
+	return refreshableVisitor(&ts), nil
 }
 
+// makes a request to the server and returns the token
 func makeRequest(serverUrl string) (*serverResponse, error) {
-	resp, err := http.Get(serverUrl)
+	ctx, cancel := context.WithTimeout(context.Background(), authServerTimeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, serverUrl, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("token request: %w", err)
+	}
+	req.Header.Add("Metadata", "true")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("token response: %w", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == 404 {
@@ -62,13 +112,50 @@ func makeRequest(serverUrl string) (*serverResponse, error) {
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("token read: %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("token error: %s", body)
 	}
 
 	var response serverResponse
 	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("token parse: %w", err)
+	}
+
+	// parse and normalize url
+	parsedHost, err := url.Parse(response.Host)
+	if err != nil {
+		return nil, fmt.Errorf("token parse: Invalid URL %s", response.Host)
+	}
+
+	response.Host = parsedHost.String()
+
+	return &response, nil
+}
+
+type authServerTokenSource string
+
+func (t authServerTokenSource) Token() (*oauth2.Token, error) {
+	token, err := makeRequest(string(t))
+
+	if err != nil {
 		return nil, err
 	}
 
-	return &response, nil
+	epoch, err := token.ExpiresOn.Int64()
+	if err != nil {
+		return nil, fmt.Errorf("token expires on: %w", err)
+	}
+
+	expiry := time.Unix(epoch, 0)
+
+	logger.Infof(context.Background(), "Refreshed access token for %s from local credentials server, which expires on %s",
+		string(t), expiry)
+
+	return &oauth2.Token{
+		AccessToken: token.AccessToken,
+		TokenType:   token.TokenType,
+		Expiry:      expiry,
+	}, nil
 }
