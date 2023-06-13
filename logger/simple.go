23bd41dab4e9162a736c8f145bcd34041b82781f
Marwan Salem <marwan.saad@incorta.com>
2023-06-14 01:33:42 +0300

    fix checking whether log level enabled using the current level, instead of the requested message level

--- logger/simple.go
+++ logger/simple.go
@@ -14,35 +14,35 @@ func (l *SimpleLogger) Enabled(_ context.Context, level Level) bool {
 }
 
 func (l *SimpleLogger) Tracef(ctx context.Context, format string, v ...any) {
-	if !l.Enabled(ctx, l.Level) {
+	if !l.Enabled(ctx, LevelTrace) {
 		return
 	}
 	log.Printf("[TRACE] "+format, v...)
 }
 
 func (l *SimpleLogger) Debugf(ctx context.Context, format string, v ...any) {
-	if !l.Enabled(ctx, l.Level) {
+	if !l.Enabled(ctx, LevelDebug) {
 		return
 	}
 	log.Printf("[DEBUG] "+format, v...)
 }
 
 func (l *SimpleLogger) Infof(ctx context.Context, format string, v ...any) {
-	if !l.Enabled(ctx, l.Level) {
+	if !l.Enabled(ctx, LevelInfo) {
 		return
 	}
 	log.Printf("[INFO] "+format, v...)
 }
 
 func (l *SimpleLogger) Warnf(ctx context.Context, format string, v ...any) {
-	if !l.Enabled(ctx, l.Level) {
+	if !l.Enabled(ctx, LevelWarn) {
 		return
 	}
 	log.Printf("[WARN] "+format, v...)
 }
 
 func (l *SimpleLogger) Errorf(ctx context.Context, format string, v ...any) {
-	if !l.Enabled(ctx, l.Level) {
+	if !l.Enabled(ctx, LevelError) {
 		return
 	}
 	log.Printf("[ERROR] "+format, v...)
