c072a43468a132e350563a8d771ea8bb122b0df3
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-06-13 18:33:31 +0200

    refreshableRetry

--- config/oauth_visitors.go
+++ config/oauth_visitors.go
@@ -1,11 +1,27 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/databricks/databricks-sdk-go/retries"
 	"golang.org/x/oauth2"
 )
+
+func retriableTokenSource(ctx context.Context, ts oauth2.TokenSource) (*oauth2.Token, error) {
+	return retries.Poll[oauth2.Token](ctx, 1*time.Minute, func() (*oauth2.Token, *retries.Err) {
+		token, err := ts.Token()
+
+		if strings.Contains(err.Error(), "throttled ...") {
+			return nil, retries.Continue(err)
+		}
+
+		return token, nil
+	})
+}
 
 func serviceToServiceVisitor(inner, cloud oauth2.TokenSource, header string) func(r *http.Request) error {
 	refreshableInner := oauth2.ReuseTokenSource(nil, inner)
