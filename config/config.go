202ba574435c5d4804529b95599cecc7c78fe333
Andrew Nester <andrew.nester.dev@gmail.com>
2023-06-21 12:05:01 +0200

    Added ClusterID field for Config (#524)
    ## Changes
    As per https://docs.databricks.com/dev-tools/databricks-connect.html
    it's possible to provide cluster ID in Databricks configuration
    
    This PR adds this field in Config struct
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [x] `make test` passing
    - [x] `make fmt` applied
    - [ ] ~relevant integration tests applied~

--- config/config.go
+++ config/config.go
@@ -38,6 +38,9 @@ type Config struct {
 	// Databricks host (either of workspace endpoint or Accounts API endpoint)
 	Host string `name:"host" env:"DATABRICKS_HOST"`
 
+	ClusterID   string `name:"cluster_id" env:"DATABRICKS_CLUSTER_ID"`
+	WarehouseID string `name:"warehouse_id" env:"DATABRICKS_WAREHOUSE_ID"`
+
 	// URL of the metadata service that provides authentication credentials.
 	MetadataServiceURL string `name:"metadata_service_url" env:"DATABRICKS_METADATA_SERVICE_URL" auth:"metadata-service,sensitive"`
 
