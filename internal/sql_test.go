cf4ac71669b522b0e1950ca45741c6ee9ca9c58e
Miles Yucht <miles@databricks.com>
2023-07-27 08:26:27 +0200

    Handle nested query parameters in Client.attempt (#559)
    ## Changes
    The Query History list API filter_by query parameter is modeled by a
    dictionary, rather than a primitive type, defining the allowed filters
    including query_start_time_range, statuses, user_ids, and warehouse_ids.
    To be compatible with gRPC transcoding, query parameters modeled by
    message types as opposed to primitives need to be separated into one
    query parameter per nested field, where the key is the path to that
    query parameter. For example:
    
    ```
    ListQueryHistoryRequest{
      FilterBy: QueryFilter{
        UserIds: []int{123, 456}
      }
    }
    ```
    becomes
    ```
    filter_by.user_ids=123&filter_by.user_ids=456
    ```
    go-querystring already flattens this but to a different form:
    ```
    filter_by[user_ids]=123&filter_by[user_ids]=456
    ```
    To fix this, we replace `[` with `.` and `]` with empty string.
    
    This resolves one of the problems from
    https://github.com/databricks/databricks-sdk-py/issues/99. The issue
    with the conflict between filter_by and next_page_token will be resolved
    by the backend service.
    
    Separately, there is an underlying issue affecting the Go SDK
    specifically, as we delegate query parameter serialization to the
    go-querystring library. This library looks at the `url` struct tag to
    name the query parameters appropriately. However, schemas defined in
    `Components` in OpenAPI are initially cached with IsQuery for all fields
    = false. As we parse operations, we incrementally discover which
    entities are present in query parameters, and we need to update the
    underlying types recursively.
    
    Lastly, this includes a bugfix to support the newest version of Delve.
    
    ## Tests
    Added an integration test for ListQueryHistory.
    
    - [x] `make test` passing
    - [x] `make fmt` applied
    - [x] relevant integration tests applied

---
+++ internal/sql_test.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go/service/sql"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAccSqlQueryHistory(t *testing.T) {
+	ctx, w := workspaceTest(t)
+	_, err := w.QueryHistory.ListAll(ctx, sql.ListQueryHistoryRequest{
+		FilterBy: &sql.QueryFilter{
+			QueryStartTimeRange: &sql.TimeRange{
+				StartTimeMs: 1690243200000, // 25-07-2023
+				EndTimeMs:   1690329600000, // 26-07-2023
+			},
+		},
+	})
+	require.NoError(t, err)
+}
--- service/sql/model.go
+++ service/sql/model.go
@@ -1676,13 +1676,13 @@ type QueryEditContent struct {
 
 // A filter to limit query history results. This field is optional.
 type QueryFilter struct {
-	QueryStartTimeRange *TimeRange `json:"query_start_time_range,omitempty"`
+	QueryStartTimeRange *TimeRange `json:"query_start_time_range,omitempty" url:"query_start_time_range,omitempty"`
 
-	Statuses []QueryStatus `json:"statuses,omitempty"`
+	Statuses []QueryStatus `json:"statuses,omitempty" url:"statuses,omitempty"`
 	// A list of user IDs who ran the queries.
-	UserIds []int `json:"user_ids,omitempty"`
+	UserIds []int `json:"user_ids,omitempty" url:"user_ids,omitempty"`
 	// A list of warehouse IDs.
-	WarehouseIds []string `json:"warehouse_ids,omitempty"`
+	WarehouseIds []string `json:"warehouse_ids,omitempty" url:"warehouse_ids,omitempty"`
 }
 
 type QueryInfo struct {
@@ -2596,9 +2596,9 @@ func (f *TerminationReasonType) Type() string {
 
 type TimeRange struct {
 	// Limit results to queries that started before this time.
-	EndTimeMs int `json:"end_time_ms,omitempty"`
+	EndTimeMs int `json:"end_time_ms,omitempty" url:"end_time_ms,omitempty"`
 	// Limit results to queries that started after this time.
-	StartTimeMs int `json:"start_time_ms,omitempty"`
+	StartTimeMs int `json:"start_time_ms,omitempty" url:"start_time_ms,omitempty"`
 }
 
 // When in synchronous mode with `wait_timeout > 0s` it determines the action
