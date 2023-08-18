c6ddfed4b8bfe17e06d0d96ea4c5460f170098e6
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-08-18 17:05:16 +0200

    Don't access `param` unless we know it's there (#582)
    Co-authored-by: Miles Yucht <miles@databricks.com>

--- openapi/code/package.go
+++ openapi/code/package.go
@@ -350,14 +350,14 @@ func (pkg *Package) Load(spec *openapi.Specification, tag openapi.Tag) error {
 			for _, list := range [][]openapi.Parameter{path.Parameters, op.Parameters} {
 				for _, v := range list {
 					param, err := pkg.resolveParam(&v)
-					if param.In == "header" {
-						continue
-					}
 					if err != nil {
-						return fmt.Errorf("no components found: %s %s", verb, prefix)
+						return fmt.Errorf("could not resolve parameter %s for %s %s. This could be due to a problem in the definition of this parameter. If using $ref, ensure that $ref is used inside the 'schema' keyword", v.Name, verb, prefix)
 					}
 					if param == nil {
 						return nil
+					}
+					if param.In == "header" {
+						continue
 					}
 					// do not propagate common path parameter to account-level APIs
 					if svc.IsAccounts && param.In == "path" && param.Name == "account_id" {
