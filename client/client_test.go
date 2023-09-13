databricks/databricks-sdk-go

commit 3c7f5724857b71550f00487ee63b7184762ef1ae

Fix nightly test failures for Go SDK (#592)
## Changes
The latest refactor of the client code in the Go SDK caused several
regressions:
1. API calls using content-type: application/json apparently don't
support transfer-encoding: chunked. To address this, I rewrite the
internals of client.go to explicitly not wrap the bytes.NewReader or
strings.NewReader in io.NopCloser. net/http's Client checks whether the
body is a byte or string reader explicitly in order to set
content-length and not use the chunked encoding.
2. UC API deletes that previously required query parameters in the body
are not correctly serialized. However, the underlying issue has been
fixed upstream, so we can remove all of this custom serialization code
now.
3. Previously, the application/json Content-Type was sent with every
request. However, there are some requests that have no request body, and
as a result, that header was not being set on the request anymore. This
conflicts with the Go SDK's implementation that always serializes the
request object and adds it to the request body. Some services tolerate a
request body without Content-Type, others do not. In this change, we
pass `nil` for `request` if there is a request body or if there are
request query parameters in the request structure (only for GET and
DELETE). This involves one small OpenAPI change: exposing the underlying
OpenAPI operation to see if there is a request body defined.
4. After changing the request body to use io.Reader, the reader is
consumed during `httpClient.Do()`. If the client needs to retry, the
request body is not reset, causing the retried request to have an empty
body. We refactor the request body implementation to support resetting.
As part of this, I made a number of changes to the error handling to
clarify method behaviors and consolidate common functionality into
simple functions.
5. Currently, we specify that the input for streaming bodies must be an

client/client.go
@@ -12,7 +12,6 @@ import (
 	"net/http"
 	"net/url"
 	"reflect"
-	"regexp"
 	"runtime"
 	"sort"
 	"strings"
@@ -69,37 +68,92 @@ type httpClient interface {
 	CloseIdleConnections()
 }
 
-// Represents a request or response Body.
+// Represents a request body.
 //
-// If the provided request data is an io.ReadCloser, DebugBytes is set to
-// "<io.ReadCloser>". Otherwise, DebugBytes is set to the marshaled JSON
+// If the provided request data is an io.Reader, DebugBytes is set to
+// "<io.Reader>". Otherwise, DebugBytes is set to the marshaled JSON
 // representation of the request data, and ReadCloser is set to a new
 // io.ReadCloser that reads from DebugBytes.
-type Body struct {
+//
+// Request bodies are never closed by the client, hence only accepting
+// io.Reader.
+type requestBody struct {
+	Reader     io.Reader
+	DebugBytes []byte
+}
+
+func newRequestBody(data any) (requestBody, error) {
+	switch v := data.(type) {
+	case io.Reader:
+		return requestBody{
+			Reader:     v,
+			DebugBytes: []byte("<io.Reader>"),
+		}, nil
+	case string:
+		return requestBody{
+			Reader:     strings.NewReader(v),
+			DebugBytes: []byte(v),
+		}, nil
+	case []byte:
+		return requestBody{
+			Reader:     bytes.NewReader(v),
+			DebugBytes: v,
+		}, nil
+	default:
+		bs, err := json.Marshal(data)
+		if err != nil {
+			return requestBody{}, fmt.Errorf("request marshal failure: %w", err)
+		}
+		return requestBody{
+			Reader:     bytes.NewReader(bs),
+			DebugBytes: bs,
+		}, nil
+	}
+}
+
+// Reset a request body to its initial state.
+//
+// This is used to retry requests with a body that has already been read. If
+// the request body is not strings.Reader or bytes.Reader, this will return an
+// error.
+func (r requestBody) reset() error {
+	if r.Reader == nil {
+		return errors.New("cannot reset nil reader")
+	}
+	if v, ok := r.Reader.(io.Seeker); ok {
+		_, err := v.Seek(0, io.SeekStart)
+		return err
+	} else {
+		return fmt.Errorf("cannot reset reader of type %T", r.Reader)
+	}
+}
+
+// Represents a response body.
+//
+// Responses must always be closed. For non-streaming responses, they are closed
+// during deserialization in the client (see unmarshall()). For streaming
+// responses, they are returned to the caller, who is responsible for closing
+// them.
+type responseBody struct {
 	ReadCloser io.ReadCloser
 	DebugBytes []byte
 }
 
-func fromBytes(bs []byte) Body {
-	return Body{
-		ReadCloser: io.NopCloser(bytes.NewReader(bs)),
-		DebugBytes: bs,
+func newResponseBody(data any) (responseBody, error) {
+	switch v := data.(type) {
+	case io.ReadCloser:
+		return responseBody{
+			ReadCloser: v,
+			DebugBytes: []byte("<io.ReadCloser>"),
+		}, nil
+	case []byte:
+		return responseBody{
+			ReadCloser: io.NopCloser(bytes.NewReader(v)),
+			DebugBytes: v,
+		}, nil
+	default:
+		return responseBody{}, errors.New("newResponseBody can only be called with io.ReadCloser or []byte")
 	}
-}
-
-func fromReader(r io.Reader) Body {
-	return Body{
-		ReadCloser: io.NopCloser(r),
-		DebugBytes: []byte("<io.Reader>"),
-	}
-}
-
-func fromJsonData(data any) (Body, error) {
-	bs, err := json.Marshal(data)
-	if err != nil {
-		return Body{}, err
-	}
-	return fromBytes(bs), nil
 }
 
 type DatabricksClient struct {
@@ -128,7 +182,7 @@ func (c *DatabricksClient) Do(ctx context.Context, method, path string,
 	return c.unmarshal(body, response)
 }
 
-func (c *DatabricksClient) unmarshal(body *Body, response any) error {
+func (c *DatabricksClient) unmarshal(body *responseBody, response any) error {
 	if response == nil {
 		return nil
 	}
@@ -145,10 +199,14 @@ func (c *DatabricksClient) unmarshal(body *Body, response any) error {
 	if len(bs) == 0 {
 		return nil
 	}
-	// If the destination is a byte slice, pass the body verbatim.
+	// If the destination is a byte slice or buffer, pass the body verbatim.
 	if raw, ok := response.(*[]byte); ok {
 		*raw = bs
 		return nil
+	}
+	if raw, ok := response.(*bytes.Buffer); ok {
+		_, err := raw.Write(bs)
+		return err
 	}
 	return json.Unmarshal(bs, &response)
 }
@@ -166,26 +224,23 @@ func (c *DatabricksClient) addHostToRequestUrl(r *http.Request) error {
 	return nil
 }
 
-func (c *DatabricksClient) fromResponse(r *http.Response) (Body, error) {
+func (c *DatabricksClient) fromResponse(r *http.Response) (responseBody, error) {
 	if r == nil {
-		return Body{}, fmt.Errorf("nil response")
+		return responseBody{}, fmt.Errorf("nil response")
 	}
 	if r.Request == nil {
-		return Body{}, fmt.Errorf("nil request")
+		return responseBody{}, fmt.Errorf("nil request")
 	}
 	streamResponse := r.Request.Header.Get("Accept") != "application/json" && r.Header.Get("Content-Type") != "application/json"
 	if streamResponse {
-		return Body{
-			ReadCloser: r.Body,
-			DebugBytes: []byte("<io.ReadCloser>"),
-		}, nil
+		return newResponseBody(r.Body)
 	}
 	defer r.Body.Close()
 	bs, err := io.ReadAll(r.Body)
 	if err != nil {
-		return Body{}, fmt.Errorf("response body: %w", err)
+		return responseBody{}, fmt.Errorf("response body: %w", err)
 	}
-	return fromBytes(bs), nil
+	return newResponseBody(bs)
 }
 
 func (c *DatabricksClient) redactedDump(prefix string, body []byte) (res string) {
@@ -194,11 +249,27 @@ func (c *DatabricksClient) redactedDump(prefix string, body []byte) (res string)
 	}.redactedDump(prefix, body)
 }
 
-func (c *DatabricksClient) wrapError(retry bool, err error) (*Body, *retries.Err) {
-	if retry {
-		return nil, retries.Continue(err)
+// Common error-handling logic for all responses that may need to be retried.
+//
+// If the error is retriable, return a retries.Err to retry the request. However, as the request body will have been consumed
+// by the first attempt, the body must be reset before retrying. If the body cannot be reset, return a retries.Err to halt.
+//
+// Always returns nil for the first parameter as there is no meaningful response body to return in the error case.
+//
+// If it is certain that an error should not be retried, use failRequest() instead.
+func (c *DatabricksClient) handleError(ctx context.Context, err *apierr.APIError, body requestBody) (*responseBody, *retries.Err) {
+	if !err.IsRetriable(ctx) {
+		return c.failRequest("non-retriable error", err)
 	}
-	return nil, retries.Halt(err)
+	if resetErr := body.reset(); resetErr != nil {
+		return nil, retries.Halt(resetErr)
+	}
+	return nil, retries.Continue(err)
+}
+
+// Fails the request with a retries.Err to halt future retries.
+func (c *DatabricksClient) failRequest(msg string, err error) (*responseBody, *retries.Err) {
+	return nil, retries.Halt(fmt.Errorf("%s: %w", msg, err))
 }
 
 func (c *DatabricksClient) attempt(
@@ -206,25 +277,25 @@ func (c *DatabricksClient) attempt(
 	method string,
 	requestURL string,
 	headers map[string]string,
-	requestBody Body,
+	requestBody requestBody,
 	visitors ...func(*http.Request) error,
-) func() (*Body, *retries.Err) {
-	return func() (*Body, *retries.Err) {
+) func() (*responseBody, *retries.Err) {
+	return func() (*responseBody, *retries.Err) {
 		err := c.rateLimiter.Wait(ctx)
 		if err != nil {
-			return nil, retries.Halt(err)
+			return c.failRequest("failed in rate limiter", err)
 		}
-		request, err := http.NewRequestWithContext(ctx, method, requestURL, requestBody.ReadCloser)
+		request, err := http.NewRequestWithContext(ctx, method, requestURL, requestBody.Reader)
+		if err != nil {
+			return c.failRequest("failed creating new request", err)
+		}
 		for k, v := range headers {
 			request.Header.Set(k, v)
-		}
-		if err != nil {
-			return c.wrapError(false, err)
 		}
 		for _, requestVisitor := range visitors {
 			err = requestVisitor(request)
 			if err != nil {
-				return c.wrapError(false, err)
+				return c.failRequest("failed during request visitor", err)
 			}
 		}
 		// request.Context() holds context potentially enhanced by visitors
@@ -232,30 +303,29 @@ func (c *DatabricksClient) attempt(
 
 		// attempt the actual request
 		response, err := c.httpClient.Do(request)
+
+		// After this point, the request body has (probably) been consumed. handleError() must be called to reset it if
+		// possible.
 		if ue, ok := err.(*url.Error); ok {
-			apiError := apierr.GenericIOError(ue)
-			return c.wrapError(apiError.IsRetriable(ctx), apiError)
+			return c.handleError(ctx, apierr.GenericIOError(ue), requestBody)
 		}
+
+		// By this point, the request body has certainly been consumed.
 		responseBody, responseBodyErr := c.fromResponse(response)
 		if responseBodyErr != nil {
-			return c.wrapError(false, apierr.ReadError(response.StatusCode, responseBodyErr))
+			return c.failRequest("failed while reading response", apierr.ReadError(response.StatusCode, responseBodyErr))
 		}
 
-		retry, err := apierr.CheckForRetry(ctx, response, responseBody.ReadCloser)
-		var apiErr *apierr.APIError
-		if err != nil && !errors.As(err, &apiErr) {
-			err = fmt.Errorf("failed request: %w", err)
-		}
+		apiErr := apierr.GetAPIError(ctx, response, responseBody.ReadCloser)
+		defer c.recordRequestLog(ctx, request, response, apiErr, requestBody.DebugBytes, responseBody.DebugBytes)
 
-		defer c.recordRequestLog(ctx, request, response, err, requestBody.DebugBytes, responseBody.DebugBytes)
-
-		if err == nil {
+		if apiErr == nil {
 			return &responseBody, nil
 		}
 
 		// proactively release the connections in HTTP connection pool
 		c.httpClient.CloseIdleConnections()
-		return c.wrapError(retry, err)
+		return c.handleError(ctx, apiErr, requestBody)
 	}
 }
 
@@ -325,7 +395,7 @@ func (c *DatabricksClient) perform(
 	headers map[string]string,
 	data interface{},
 	visitors ...func(*http.Request) error,
-) (*Body, error) {
+) (*responseBody, error) {
 	// replace double slash in the request URL with a single slash
 	requestURL = strings.Replace(requestURL, "//", "/", -1)
 	requestBody, err := makeRequestBody(method, &requestURL, data)
@@ -391,87 +461,19 @@ func makeQueryString(data interface{}) (string, error) {
 	return "", fmt.Errorf("unsupported query string data: %#v", data)
 }
 
-// Remove all custom request serializer logic once APP-1331 is rolled out.
-type serializer func(any) (Body, error)
-
-func serializeQueryParamsToRequestBody(data any) (Body, error) {
-	m := map[string]any{}
-	rv := reflect.ValueOf(data)
-	rt := reflect.TypeOf(data)
-	// If data is a map, just serialize it to JSON.
-	if rv.Kind() == reflect.Map {
-		return fromJsonData(data)
-	}
-	for i := 0; i < rv.NumField(); i++ {
-		field := rv.Field(i)
-		tag := rt.Field(i).Tag.Get("url")
-		if tag == "" {
-			continue
-		}
-		value := field.Interface()
-		if field.IsZero() {
-			continue
-		}
-		key := strings.Split(tag, ",")[0]
-		m[key] = value
-	}
-
-	return fromJsonData(data)
-}
-
-// List of (method, URL) pairs whose request bodies need to be serialized in a
-// custom way. These can be removed after APP-1331 is rolled out.
-var requestInBodyOverrides = []struct {
-	method     string
-	urlRegexp  *regexp.Regexp
-	serializer serializer
-}{
-	{"DELETE", regexp.MustCompile("(/api/2.1)?/unity-catalog/(metastores|catalogs)/[^/]+"), serializeQueryParamsToRequestBody},
-	{"DELETE", regexp.MustCompile("(/api/2.1)?/unity-catalog/workspaces/[^/]+/(metastore|catalog)"), serializeQueryParamsToRequestBody},
-}
-
-func getRequestCustomSerializer(method string, requestURL *string) serializer {
-	for _, override := range requestInBodyOverrides {
-		// Return true if the request URL has the prefix and no trailing segments
-		// which would indicate other APIs.
-		matchRequestMethod := method == override.method
-		matchRequestURL := override.urlRegexp.MatchString(*requestURL)
-		if matchRequestMethod && matchRequestURL {
-			return override.serializer
-		}
-	}
-	return nil
-}
-
-func makeRequestBody(method string, requestURL *string, data interface{}) (Body, error) {
-	if data == nil && (method == "DELETE" || method == "GET") {
-		return Body{}, nil
-	}
-	if customSerializer := getRequestCustomSerializer(method, requestURL); customSerializer != nil {
-		return customSerializer(data)
+func makeRequestBody(method string, requestURL *string, data interface{}) (requestBody, error) {
+	if data == nil {
+		return requestBody{}, nil
 	}
 	if method == "GET" || method == "DELETE" {
 		qs, err := makeQueryString(data)
 		if err != nil {
-			return Body{}, err
+			return requestBody{}, err
 		}
 		*requestURL += qs
-		return fromBytes([]byte{}), nil
+		return newRequestBody([]byte{})
 	}
-	if bytes, ok := data.([]byte); ok {
-		return fromBytes(bytes), nil
-	}
-	if reader, ok := data.(io.Reader); ok {
-		return fromReader(reader), nil
-	}
-	if str, ok := data.(string); ok {
-		return fromBytes([]byte(str)), nil
-	}
-	res, err := fromJsonData(data)
-	if err != nil {
-		return Body{}, fmt.Errorf("request marshal failure: %w", err)
-	}
-	return res, nil
+	return newRequestBody(data)
 }
 
 func orDefault(configured, _default int) int {

client/client_test.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -71,7 +73,7 @@ func TestSimpleRequestFailsURLError(t *testing.T) {
 	}, map[string]string{
 		"c": "d",
 	}, nil)
-	assert.EqualError(t, err, "GET \"/a/b\": nope")
+	assert.EqualError(t, err, "non-retriable error: GET \"/a/b\": nope")
 }
 
 func TestSimpleRequestFailsAPIError(t *testing.T) {
@@ -100,7 +102,7 @@ func TestSimpleRequestFailsAPIError(t *testing.T) {
 	}, map[string]string{
 		"c": "d",
 	}, nil)
-	assert.EqualError(t, err, "nope")
+	assert.EqualError(t, err, "non-retriable error: nope")
 }
 
 func TestSimpleRequestSucceeds(t *testing.T) {
@@ -165,10 +167,12 @@ func TestHaltAttemptForLimit(t *testing.T) {
 	c := &DatabricksClient{
 		rateLimiter: &rate.Limiter{},
 	}
-	_, rerr := c.attempt(ctx, "GET", "foo", nil, fromBytes([]byte{}))()
+	req, err := newRequestBody([]byte{})
+	assert.NoError(t, err)
+	_, rerr := c.attempt(ctx, "GET", "foo", nil, req)()
 	assert.NotNil(t, rerr)
 	assert.Equal(t, true, rerr.Halt)
-	assert.EqualError(t, rerr.Err, "rate: Wait(n=1) exceeds limiter's burst 0")
+	assert.EqualError(t, rerr.Err, "failed in rate limiter: rate: Wait(n=1) exceeds limiter's burst 0")
 }
 
 func TestHaltAttemptForNewRequest(t *testing.T) {
@@ -176,10 +180,12 @@ func TestHaltAttemptForNewRequest(t *testing.T) {
 	c := &DatabricksClient{
 		rateLimiter: rate.NewLimiter(rate.Inf, 1),
 	}
-	_, rerr := c.attempt(ctx, "🥱", "/", nil, fromBytes([]byte{}))()
+	req, err := newRequestBody([]byte{})
+	assert.NoError(t, err)
+	_, rerr := c.attempt(ctx, "🥱", "/", nil, req)()
 	assert.NotNil(t, rerr)
 	assert.Equal(t, true, rerr.Halt)
-	assert.EqualError(t, rerr.Err, `net/http: invalid method "🥱"`)
+	assert.EqualError(t, rerr.Err, `failed creating new request: net/http: invalid method "🥱"`)
 }
 
 func TestHaltAttemptForVisitor(t *testing.T) {
@@ -187,13 +193,15 @@ func TestHaltAttemptForVisitor(t *testing.T) {
 	c := &DatabricksClient{
 		rateLimiter: rate.NewLimiter(rate.Inf, 1),
 	}
-	_, rerr := c.attempt(ctx, "GET", "/", nil, fromBytes([]byte{}),
+	req, err := newRequestBody([]byte{})
+	assert.NoError(t, err)
+	_, rerr := c.attempt(ctx, "GET", "/", nil, req,
 		func(r *http.Request) error {
 			return fmt.Errorf("🥱")
 		})()
 	assert.NotNil(t, rerr)
 	assert.Equal(t, true, rerr.Halt)
-	assert.EqualError(t, rerr.Err, "🥱")
+	assert.EqualError(t, rerr.Err, "failed during request visitor: 🥱")
 }
 
 func TestMakeRequestBody(t *testing.T) {
@@ -203,7 +211,7 @@ func TestMakeRequestBody(t *testing.T) {
 	requestURL := "/a/b/c"
 	body, err := makeRequestBody("GET", &requestURL, x{"test"})
 	assert.NoError(t, err)
-	bodyBytes, err := io.ReadAll(body.ReadCloser)
+	bodyBytes, err := io.ReadAll(body.Reader)
 	assert.NoError(t, err)
 	assert.Equal(t, "/a/b/c?scope=test", requestURL)
 	assert.Equal(t, 0, len(bodyBytes))
@@ -211,7 +219,7 @@ func TestMakeRequestBody(t *testing.T) {
 	requestURL = "/a/b/c"
 	body, err = makeRequestBody("POST", &requestURL, x{"test"})
 	assert.NoError(t, err)
-	bodyBytes, err = io.ReadAll(body.ReadCloser)
+	bodyBytes, err = io.ReadAll(body.Reader)
 	assert.NoError(t, err)
 	assert.Equal(t, "/a/b/c", requestURL)
 	x1 := `{"scope":"test"}`
@@ -222,7 +230,7 @@ func TestMakeRequestBodyFromReader(t *testing.T) {
 	requestURL := "/a/b/c"
 	body, err := makeRequestBody("PUT", &requestURL, strings.NewReader("abc"))
 	assert.NoError(t, err)
-	bodyBytes, err := io.ReadAll(body.ReadCloser)
+	bodyBytes, err := io.ReadAll(body.Reader)
 	assert.NoError(t, err)
 	assert.Equal(t, []byte("abc"), bodyBytes)
 }
@@ -314,7 +322,7 @@ func TestSimpleRequestErrReaderBody(t *testing.T) {
 	}
 	headers := map[string]string{"Accept": "application/json"}
 	err := c.Do(context.Background(), "PATCH", "/a", headers, map[string]any{}, nil)
-	assert.EqualError(t, err, "response body: test error")
+	assert.EqualError(t, err, "failed while reading response: response body: test error")
 }
 
 func TestSimpleRequestErrReaderBodyStreamResponse(t *testing.T) {
@@ -352,7 +360,7 @@ func TestSimpleRequestErrReaderCloseBody(t *testing.T) {
 	}
 	headers := map[string]string{"Accept": "application/json"}
 	err := c.Do(context.Background(), "PATCH", "/a", headers, map[string]any{}, nil)
-	assert.EqualError(t, err, "response body: test error")
+	assert.EqualError(t, err, "failed while reading response: response body: test error")
 }
 
 func TestSimpleRequestErrReaderCloseBody_StreamResponse(t *testing.T) {
@@ -551,4 +559,75 @@ func TestInlineArrayDebugging_StreamResponse(t *testing.T) {
 	assert.Equal(t, `[DEBUG] GET /a?a=3&b=0&c=23
 <  
 < [non-JSON document of 15 bytes]. <io.ReadCloser>`, bufLogger.String())
+}
+
+func TestStreamRequestFromFileWithReset(t *testing.T) {
+	// make a temporary file with some content
+	f, err := os.CreateTemp("", "databricks-client-test")
+	assert.NoError(t, err)
+	defer os.Remove(f.Name())
+	_, err = f.WriteString("hello world")
+	assert.NoError(t, err)
+	assert.NoError(t, f.Close())
+
+	// Make a reader that reads this file
+	r, err := os.Open(f.Name())
+	assert.NoError(t, err)
+	defer r.Close()
+
+	succeed := false
+	handler := func(req *http.Request) (*http.Response, error) {
+		bytes, err := io.ReadAll(req.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "hello world", string(bytes))
+		if succeed {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(strings.NewReader("succeeded")),
+				Request:    req,
+			}, nil
+		}
+		succeed = true
+		return &http.Response{
+			StatusCode: 429,
+			Body:       io.NopCloser(strings.NewReader("failed")),
+			Request:    req,
+		}, nil
+	}
+
+	client := &DatabricksClient{
+		httpClient:   hc(handler),
+		rateLimiter:  rate.NewLimiter(rate.Limit(1), 1),
+		Config:       config.NewMockConfig(func(r *http.Request) error { return nil }),
+		retryTimeout: time.Hour,
+	}
+
+	respBytes := bytes.Buffer{}
+	err = client.Do(context.Background(), "POST", "/a", nil, r, &respBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, "succeeded", respBytes.String())
+	assert.True(t, succeed)
+}
+
+type customReader struct{}
+
+func (c customReader) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func TestCannotRetryArbitraryReader(t *testing.T) {
+	client := &DatabricksClient{
+		httpClient: hc(func(r *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 429,
+				Request:    r,
+				Body:       io.NopCloser(strings.NewReader("")),
+			}, nil
+		}),
+		rateLimiter:  rate.NewLimiter(rate.Limit(1), 1),
+		Config:       config.NewMockConfig(func(r *http.Request) error { return nil }),
+		retryTimeout: time.Hour,
+	}
+	err := client.Do(context.Background(), "POST", "/a", nil, customReader{}, nil)
+	assert.ErrorContains(t, err, "cannot reset reader of type client.customReader")
 }

.codegen/impl.go.tmpl
@@ -32,7 +32,7 @@ func (a *{{.Service.CamelName}}Impl) {{.PascalName}}(ctx context.Context{{if .Re
 {{end}}
 
 {{ define "request-param" -}}
-  {{ if .Request -}}
+  {{ if or (and .Request (or (eq .Verb "GET") (eq .Verb "DELETE"))) (and .Operation .Operation.RequestBody) -}}
     request{{ if .RequestBodyField }}.{{.RequestBodyField.PascalName}}{{end}}
   {{- else }}nil
   {{- end }}
@@ -60,7 +60,7 @@ func (a *{{.Service.CamelName}}Impl) {{.PascalName}}(ctx context.Context{{if .Re
 {{ define "response" -}}
   {{ if .Response -}}
   {{ if .IsResponseByteStream -}}
-    &{{ .Response.PascalName }}{ {{ .ResponseBodyField.CamelName }}: {{ .Response.CamelName }} }
+    &{{ .Response.PascalName }}{ {{ .ResponseBodyField.PascalName }}: {{ .Response.CamelName }} }
   {{- else -}}
     {{ if not .Response.ArrayValue }}&{{end}}{{.Response.CamelName}}
   {{- end }}, {{end}}err

apierr/errors.go
@@ -74,6 +74,9 @@ func (apiError *APIError) IsTooManyRequests() bool {
 
 // isRetriable returns true if error is retriable
 func (apiError *APIError) IsRetriable(ctx context.Context) bool {
+	if apiError.IsTooManyRequests() {
+		return true
+	}
 	// Handle transient errors for retries
 	for _, substring := range transientErrorStringMatches {
 		if strings.Contains(apiError.Message, substring) {
@@ -116,29 +119,23 @@ func GenericIOError(ue *url.Error) *APIError {
 		StatusCode: 523,
 		Message:    ue.Error(),
 	}
-
 }
 
-// CheckForRetry inspects HTTP errors from the Databricks API for known transient errors on Workspace creation
-func CheckForRetry(ctx context.Context, resp *http.Response, body io.ReadCloser) (bool, error) {
-	if resp == nil {
-		// If response is nil we can't make retry choices.
-		// In this case don't retry and return the original error from httpclient
-		return false, nil
-	}
+// GetAPIError inspects HTTP errors from the Databricks API for known transient errors.
+func GetAPIError(ctx context.Context, resp *http.Response, body io.ReadCloser) *APIError {
 	if resp.StatusCode == 429 {
-		return true, TooManyRequests()
+		return TooManyRequests()
 	}
 	if resp.StatusCode >= 400 {
 		// read in response body as it is actually an error
 		bodyBytes, err := io.ReadAll(body)
 		if err != nil {
-			return false, ReadError(resp.StatusCode, err)
+			return ReadError(resp.StatusCode, err)
 		}
 		apiError := parseErrorFromResponse(resp, bodyBytes)
-		return apiError.IsRetriable(ctx), apiError
+		return apiError
 	}
-	return false, nil
+	return nil
 }
 
 func parseErrorFromResponse(resp *http.Response, body []byte) *APIError {

internal/files_test.go
@@ -74,7 +74,7 @@ func TestAccDbfsOpen(t *testing.T) {
 	// Upload through [io.Writer] should fail because the file exists.
 	{
 		_, err := w.Dbfs.Open(ctx, path, files.FileModeWrite)
-		require.ErrorContains(t, err, "dbfs open: A file or directory already exists at the input path")
+		require.ErrorContains(t, err, "dbfs open: non-retriable error: A file or directory already exists at the input path")
 	}
 
 	// Upload through [io.ReadFrom] with overwrite bit set.
@@ -141,11 +141,11 @@ func TestAccDbfsOpenDirectory(t *testing.T) {
 
 	// Try to open the directory for writing.
 	_, err = w.Dbfs.Open(ctx, path, files.FileModeWrite)
-	assert.ErrorContains(t, err, "dbfs open: A file or directory already exists")
+	assert.ErrorContains(t, err, "dbfs open: non-retriable error: A file or directory already exists")
 
 	// Try to open the directory for writing with overwrite flag set.
 	_, err = w.Dbfs.Open(ctx, path, files.FileModeWrite|files.FileModeOverwrite)
-	assert.ErrorContains(t, err, "dbfs open: A file or directory already exists")
+	assert.ErrorContains(t, err, "dbfs open: non-retriable error: A file or directory already exists")
 }
 
 func TestAccDbfsReadFileWriteFile(t *testing.T) {

openapi/code/load_test.go
@@ -103,7 +103,7 @@ func TestMethodsReport(t *testing.T) {
 					m.KebabName(),
 					methodWithoutService,
 					singleService,
-					m.operation.Crud,
+					m.Operation.Crud,
 					strings.Join(fields, ", "),
 				))
 			}

commit 584e7e09eb067bee7c07e77052fad0a77a179786

Fix case where retry of a request without body fails (#614)
## Changes

This problem was introduced in #592. It contains an assumption that a
`nil` body on the newly introduced `requestBody` struct cannot be `nil`,
yet it can be for requests without a body (HEAD, GET, and DELETE). If a
request of one of these types fails with a retriable error (e.g. a 429),
the code tries to reset the body, which in turn fails because it is nil.

The symptom is a GET request failing with: `cannot reset nil reader`.

Because a nil request body is valid, this change updates the expectation
accordingly.

## Tests

- [x] `make test` passing
- [x] `make fmt` applied
- [x] relevant integration tests applied

client/client.go
@@ -113,12 +113,12 @@ func newRequestBody(data any) (requestBody, error) {
 
 // Reset a request body to its initial state.
 //
-// This is used to retry requests with a body that has already been read. If
-// the request body is not strings.Reader or bytes.Reader, this will return an
-// error.
+// This is used to retry requests with a body that has already been read.
+// If the request body is not resettable (i.e. not nil and of type other than
+// strings.Reader or bytes.Reader), this will return an error.
 func (r requestBody) reset() error {
 	if r.Reader == nil {
-		return errors.New("cannot reset nil reader")
+		return nil
 	}
 	if v, ok := r.Reader.(io.Seeker); ok {
 		_, err := v.Seek(0, io.SeekStart)

client/client_test.go
@@ -637,3 +637,39 @@ func TestCannotRetryArbitraryReader(t *testing.T) {
 	err := client.Do(context.Background(), "POST", "/a", nil, customReader{}, nil)
 	assert.ErrorContains(t, err, "cannot reset reader of type client.customReader")
 }
+
+func TestRetryGetRequest(t *testing.T) {
+	// This test was added in response to https://github.com/databricks/terraform-provider-databricks/issues/2675.
+	succeed := false
+	handler := func(req *http.Request) (*http.Response, error) {
+		assert.Nil(t, req.Body)
+
+		if succeed {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(strings.NewReader("succeeded")),
+				Request:    req,
+			}, nil
+		}
+
+		succeed = true
+		return &http.Response{
+			StatusCode: 429,
+			Body:       io.NopCloser(strings.NewReader("failed")),
+			Request:    req,
+		}, nil
+	}
+
+	client := &DatabricksClient{
+		httpClient:   hc(handler),
+		rateLimiter:  rate.NewLimiter(rate.Limit(1), 1),
+		Config:       config.NewMockConfig(func(r *http.Request) error { return nil }),
+		retryTimeout: time.Hour,
+	}
+
+	respBytes := bytes.Buffer{}
+	err := client.Do(context.Background(), "GET", "/a", nil, nil, &respBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, "succeeded", respBytes.String())
+	assert.True(t, succeed)
+}