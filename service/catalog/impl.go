a244d6393abff2a0c3421b1600e6ef0b39b0df1e
Miles Yucht <miles@databricks.com>
2023-10-03 09:51:36 +0200

    Update OpenAPI Spec with Go SDK

--- service/catalog/impl.go
+++ service/catalog/impl.go
@@ -41,13 +41,13 @@ func (a *accountMetastoreAssignmentsImpl) Get(ctx context.Context, request GetAc
 	return &accountsMetastoreAssignment, err
 }
 
-func (a *accountMetastoreAssignmentsImpl) List(ctx context.Context, request ListAccountMetastoreAssignmentsRequest) ([]int64, error) {
-	var workspaceIdList []int64
+func (a *accountMetastoreAssignmentsImpl) List(ctx context.Context, request ListAccountMetastoreAssignmentsRequest) (*ListAccountMetastoreAssignmentsResponse, error) {
+	var listAccountMetastoreAssignmentsResponse ListAccountMetastoreAssignmentsResponse
 	path := fmt.Sprintf("/api/2.0/accounts/%v/metastores/%v/workspaces", a.client.ConfiguredAccountID(), request.MetastoreId)
 	headers := make(map[string]string)
 	headers["Accept"] = "application/json"
-	err := a.client.Do(ctx, http.MethodGet, path, headers, request, &workspaceIdList)
-	return workspaceIdList, err
+	err := a.client.Do(ctx, http.MethodGet, path, headers, request, &listAccountMetastoreAssignmentsResponse)
+	return &listAccountMetastoreAssignmentsResponse, err
 }
 
 func (a *accountMetastoreAssignmentsImpl) Update(ctx context.Context, request AccountsUpdateMetastoreAssignment) error {
