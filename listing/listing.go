320c7fe4910091e7688c6caaac2b01f1f718450a
Miles Yucht <miles@databricks.com>
2023-10-19 15:37:00 +0200

    Implement ListAll in terms of List

--- listing/listing.go
+++ listing/listing.go
@@ -5,12 +5,31 @@ import (
 	"errors"
 )
 
+type ListingStatus int
+
+const (
+	// ListingStatusExhausted indicates that the iterator is exhausted and
+	// there are no more items to be fetched.
+	ListingStatusExhausted ListingStatus = iota
+	// ListingStatusNotExhausted indicates that the iterator is not exhausted
+	// and there are more items to be fetched.
+	ListingStatusNotExhausted ListingStatus = iota
+	// ListingStatusCheckResult indicates that the iterator is exhausted if and
+	// only if there are no results returned.
+	ListingStatusCheckResult ListingStatus = iota
+)
+
 // Use struct{} for Req to indicate one-shot iterator.
 type Iterator[Req, Resp, T any] struct {
-	nextReq        Req
-	getNextPage    func(context.Context, Req) (Resp, error)
-	getItems       func(Resp) []T
-	getNextReq     func(Resp) Req
+	nextReq     Req
+	getNextPage func(context.Context, Req) (Resp, error)
+	getItems    func(Resp) []T
+
+	// getNextReq is used to get the next request to be used in the next page.
+	// The returned boolean value indicates whether the iterator is exhausted
+	// without needing to fetch the next page (e.g. token-based pagination where
+	// no next page token is provided in the response).
+	getNextReq     func(Resp) (Req, ListingStatus)
 	currentPage    []T
 	currentPageIdx int
 	isExhausted    bool
@@ -22,7 +41,7 @@ func NewIterator[Req, Resp, T any](
 	nextReq Req,
 	getNextPage func(context.Context, Req) (Resp, error),
 	getItems func(Resp) []T,
-	getNextReq func(Resp) Req,
+	getNextReq func(Resp) (Req, ListingStatus),
 ) *Iterator[Req, Resp, T] {
 	return &Iterator[Req, Resp, T]{
 		nextReq:     nextReq,
@@ -47,8 +66,9 @@ func (i *Iterator[Req, Resp, T]) loadNextPageIfNeeded(ctx context.Context) (bool
 	items := i.getItems(resp)
 	i.currentPage = items
 	i.currentPageIdx = 0
-	i.nextReq = i.getNextReq(resp)
-	if !i.isExhausted && len(items) == 0 {
+	var status ListingStatus
+	i.nextReq, status = i.getNextReq(resp)
+	if !i.isExhausted && (status == ListingStatusExhausted || (status == ListingStatusCheckResult && len(items) == 0)) {
 		i.isExhausted = true
 	}
 	return !i.isExhausted, err
