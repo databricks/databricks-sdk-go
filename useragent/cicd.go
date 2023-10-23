d8bd75de93f07310f09257f93659a4396ad4c575
monalisa <shreyas.goenka@databricks.com>
2023-10-23 13:50:00 +0200

    add function for env detect fixtureS

--- useragent/cicd.go
+++ useragent/cicd.go
@@ -16,19 +16,20 @@ type cicdProvider struct {
 	envVars []envVar
 }
 
-// cicdProviders is a list of CI/CD providers and their env vars we can rely on
-// to detect them.
-var cicdProviders = []cicdProvider{
-	{"github", []envVar{{"GITHUB_ACTIONS", "true"}}},
-	{"gitlab", []envVar{{"GITLAB_CI", "true"}}},
-	{"jenkins", []envVar{{"JENKINS_URL", ""}}},
-	{"azure-devops", []envVar{{"TF_BUILD", "True"}}},
-	{"circle", []envVar{{"CIRCLECI", "true"}}},
-	{"travis", []envVar{{"TRAVIS", "true"}}},
-	{"bitbucket", []envVar{{"BITBUCKET_BUILD_NUMBER", ""}}},
-	{"google-cloud-build", []envVar{{"PROJECT_ID", ""}, {"BUILD_ID", ""}, {"PROJECT_NUMBER", ""}, {"LOCATION", ""}}},
-	{"aws-code-build", []envVar{{"CODEBUILD_BUILD_ARN", ""}}},
-	{"tf-cloud", []envVar{{"TFC_RUN_ID", ""}}},
+// List of CI/CD providers and their env vars we can rely on to detect them.
+func listCiCdProviders() []cicdProvider {
+	return []cicdProvider{
+		{"github", []envVar{{"GITHUB_ACTIONS", "true"}}},
+		{"gitlab", []envVar{{"GITLAB_CI", "true"}}},
+		{"jenkins", []envVar{{"JENKINS_URL", ""}}},
+		{"azure-devops", []envVar{{"TF_BUILD", "True"}}},
+		{"circle", []envVar{{"CIRCLECI", "true"}}},
+		{"travis", []envVar{{"TRAVIS", "true"}}},
+		{"bitbucket", []envVar{{"BITBUCKET_BUILD_NUMBER", ""}}},
+		{"google-cloud-build", []envVar{{"PROJECT_ID", ""}, {"BUILD_ID", ""}, {"PROJECT_NUMBER", ""}, {"LOCATION", ""}}},
+		{"aws-code-build", []envVar{{"CODEBUILD_BUILD_ARN", ""}}},
+		{"tf-cloud", []envVar{{"TFC_RUN_ID", ""}}},
+	}
 }
 
 // detect returns true if all env vars are set and have expected values.
@@ -48,7 +49,7 @@ func (p cicdProvider) detect() bool {
 // CiCdProvider returns the name of the CI/CD provider if detected. Returns the
 // first one, if multiple are detected.
 func CiCdProvider() string {
-	for _, p := range cicdProviders {
+	for _, p := range listCiCdProviders() {
 		if p.detect() {
 			return p.name
 		}
