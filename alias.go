e7c9c5c65b4b97a8c23fcf072f8f13570d311228
Serge Smertin <serge.smertin@databricks.com>
2023-11-07 16:56:27 +0100

    Introduce more specific exceptions, like `errors.Is(err, databricks.ErrNotFound)`, `errors.Is(err, databricks.ErrAlreadyExists)`, `errors.Is(err, databricks.ErrBadRequest)`, `errors.Is(err, databricks.ErrPermissionDenied)`, `errors.Is(err, databricks.ErrInternal)`, and others
    This PR enables ergonomic error handling for all Databricks API Errors, following the same principles as https://github.com/databricks/databricks-sdk-py/pull/376

--- alias.go
+++ alias.go
@@ -1,12 +1,34 @@
 package databricks
 
 import (
+	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/config"
 	"github.com/databricks/databricks-sdk-go/useragent"
 	"github.com/databricks/databricks-sdk-go/version"
 )
 
 type Config config.Config
+
+var (
+	ErrInternal                = apierr.ErrInternal
+	ErrDataLoss                = apierr.ErrDataLoss
+	ErrUnknown                 = apierr.ErrUnknown
+	ErrTemporarilyUnavailable  = apierr.ErrTemporarilyUnavailable
+	ErrBadRequest              = apierr.ErrBadRequest
+	ErrInvalidParameterValue   = apierr.ErrInvalidParameterValue
+	ErrDeadlineExceeded        = apierr.ErrDeadlineExceeded
+	ErrCancelled               = apierr.ErrCancelled
+	ErrNotFound                = apierr.ErrNotFound
+	ErrUnauthenticated         = apierr.ErrUnauthenticated
+	ErrPermissionDenied        = apierr.ErrPermissionDenied
+	ErrTooManyRequests         = apierr.ErrTooManyRequests
+	ErrResourceExhausted       = apierr.ErrResourceExhausted
+	ErrRequestLimitExceeded    = apierr.ErrRequestLimitExceeded
+	ErrResourceConflict        = apierr.ErrResourceConflict
+	ErrAlreadyExists           = apierr.ErrAlreadyExists
+	ErrAborted                 = apierr.ErrAborted
+	ErrOperationNotImplemented = apierr.ErrOperationNotImplemented
+)
 
 // Must panics if error is not nil. It's intended to be used with
 // [databricks.NewWorkspaceClient] and [databricks.NewAccountClient].
---
+++ apierr/unwrap_test.go
@@ -0,0 +1,73 @@
+package apierr
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+var testTable = []mapping{
+	statusMapping(400, ErrBadRequest),
+	codeMapping(400, "INVALID_PARAMETER_VALUE", ErrBadRequest),
+	codeMapping(400, "INVALID_PARAMETER_VALUE", ErrInvalidParameterValue),
+	codeMapping(400, "REQUEST_LIMIT_EXCEEDED", ErrTooManyRequests),
+	statusMapping(401, ErrUnauthenticated),
+	statusMapping(401, os.ErrPermission),
+	statusMapping(403, ErrPermissionDenied),
+	statusMapping(403, os.ErrPermission),
+	statusMapping(404, ErrNotFound),
+	statusMapping(404, os.ErrNotExist),
+	statusMapping(409, ErrResourceConflict),
+	codeMapping(409, "ABORTED", ErrAborted),
+	codeMapping(409, "ABORTED", ErrResourceConflict),
+	codeMapping(409, "ALREADY_EXISTS", ErrAlreadyExists),
+	codeMapping(409, "ALREADY_EXISTS", ErrResourceConflict),
+	statusMapping(429, ErrTooManyRequests),
+	codeMapping(429, "REQUEST_LIMIT_EXCEEDED", ErrTooManyRequests),
+	codeMapping(429, "REQUEST_LIMIT_EXCEEDED", ErrRequestLimitExceeded),
+	codeMapping(429, "RESOURCE_EXHAUSTED", ErrTooManyRequests),
+	codeMapping(429, "RESOURCE_EXHAUSTED", ErrResourceExhausted),
+	statusMapping(499, ErrCancelled),
+	statusMapping(500, ErrInternal),
+	codeMapping(500, "UNKNOWN", ErrInternal),
+	codeMapping(500, "UNKNOWN", ErrUnknown),
+	codeMapping(500, "DATA_LOSS", ErrInternal),
+	codeMapping(500, "DATA_LOSS", ErrDataLoss),
+	statusMapping(501, ErrOperationNotImplemented),
+	statusMapping(503, ErrTemporarilyUnavailable),
+	statusMapping(504, ErrDeadlineExceeded),
+	statusMapping(504, os.ErrDeadlineExceeded),
+}
+
+func TestErrorMapping(t *testing.T) {
+	for _, m := range testTable {
+		if !errors.Is(m.APIError, m.mapTo) {
+			t.Errorf("error_code='%s', http_status='%d', expected to unwrap to `%s`",
+				m.ErrorCode, m.StatusCode, m.mapTo)
+		}
+	}
+}
+
+type mapping struct {
+	*APIError
+	mapTo error
+}
+
+func statusMapping(statusCode int, err error) mapping {
+	return mapping{
+		APIError: &APIError{
+			StatusCode: statusCode,
+		},
+		mapTo: err,
+	}
+}
+
+func codeMapping(statusCode int, errorCode string, err error) mapping {
+	return mapping{
+		APIError: &APIError{
+			StatusCode: statusCode,
+			ErrorCode:  errorCode,
+		},
+		mapTo: err,
+	}
+}
