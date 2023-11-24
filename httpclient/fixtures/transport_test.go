ff983e257ba2bb2fd9f1c61d54541315fa3fad5a
Serge Smertin <serge.smertin@databricks.com>
2023-11-24 17:52:40 +0100

    Port `qa.HTTPFixtures` to faster transport-level stubs

---
+++ httpclient/fixtures/transport_test.go
@@ -0,0 +1,167 @@
+package fixtures_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go/httpclient"
+	"github.com/databricks/databricks-sdk-go/httpclient/fixtures"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetErrorStub(t *testing.T) {
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "GET", "/foo", httpclient.WithRequestData(map[string]any{
+		"foo": "bar",
+	}))
+	// spacing in this test is very important
+	assert.EqualError(t, err, `Get "/foo?foo=bar": missing stub, please add: {
+    Method:   "GET",
+    Resource: "/foo?foo=bar",
+    Response: XXX {
+        // fill in specific fields...
+    },
+},
+`, err.Error())
+}
+
+func TestGetErrorStubFilled(t *testing.T) {
+	var out struct {
+		Foo string `json:"foo"`
+	}
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{
+			{
+				Method:   "GET",
+				Resource: "/foo?foo=bar",
+				Response: map[string]any{
+					"foo": "bar",
+				},
+			},
+		},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "GET", "/foo",
+		httpclient.WithResponseUnmarshal(&out),
+		httpclient.WithRequestData(map[string]any{
+			"foo": "bar",
+		}))
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", out.Foo)
+}
+
+func TestReplyWithString(t *testing.T) {
+	var out struct {
+		Foo string `json:"foo"`
+	}
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{
+			{
+				Method:   "GET",
+				Resource: "/foo?foo=bar",
+				Response: `{"foo": "bar"}`,
+			},
+		},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "GET", "/foo",
+		httpclient.WithResponseUnmarshal(&out),
+		httpclient.WithRequestData(map[string]any{
+			"foo": "bar",
+		}))
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", out.Foo)
+}
+
+func TestGetErrorStubWithHost(t *testing.T) {
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "GET", "http://localhost:1234/foo", httpclient.WithRequestData(map[string]any{
+		"foo": "bar",
+	}))
+	// spacing in this test is very important
+	assert.EqualError(t, err, `Get "http://localhost:1234/foo?foo=bar": missing stub, please add: {
+    Method:   "GET",
+    Resource: "/foo?foo=bar",
+    Response: XXX {
+        // fill in specific fields...
+    },
+},
+`, err.Error())
+}
+
+func TestPostErrorStub(t *testing.T) {
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "POST", "/foo", httpclient.WithRequestData(map[string]any{
+		"foo": "bar",
+	}))
+	// spacing in this test is very important
+	assert.EqualError(t, err, `Post "/foo": missing stub, please add: {
+    Method:   "POST",
+    Resource: "/foo",
+    ExpectedRequest: XXX {
+        Foo: "bar",
+    },
+    // ExpectedRequest: map[string]interface {}{"foo":"bar"},
+    Response: XXX {
+        // fill in specific fields...
+    },
+},
+`)
+}
+
+func TestPostErrorStubFilled(t *testing.T) {
+	type op struct {
+		Foo string `json:"foo"`
+	}
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{
+			{
+				Method:   "POST",
+				Resource: "/foo",
+				ExpectedRequest: op{
+					Foo: "bar",
+				},
+			},
+		},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "POST", "/foo", httpclient.WithRequestData(map[string]any{
+		"foo": "bar",
+	}))
+	assert.NoError(t, err)
+}
+
+func TestPostUnexpectedRequest(t *testing.T) {
+	type op struct {
+		Foo string `json:"foo"`
+	}
+	client := httpclient.NewApiClient(httpclient.ClientConfig{
+		Transport: fixtures.HTTPFixtures{
+			{
+				Method:   "POST",
+				Resource: "/foo",
+				ExpectedRequest: op{
+					Foo: "ba..r",
+				},
+			},
+		},
+	})
+	ctx := context.Background()
+	err := client.Do(ctx, "POST", "/foo", httpclient.WithRequestData(map[string]any{
+		"foo": "bar",
+	}))
+	assert.EqualError(t, err, `Post "/foo": expected request: want {
+  "foo": "ba..r"
+}, got {
+  "foo": "bar"
+}`, err.Error())
+}
