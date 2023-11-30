databricks/databricks-sdk-go

commit b0f3c83d9f5eca12dbd4c1daa03e06aff98a94ff

Decouple HTTP retries and error mapping mechanics from `DatabricksClient` into `httpclient.ApiClient` (#699)
## Changes

This PR decouples resiliency of HTTP client (rate limiting, retries,
timeouts, debug logging) from `client.DatabricksClient` and moves it to
a dedicated class, allowing to later use the same robust mechanism to
refresh AAD tokens.

What's changed in `httpclient.ApiClient` compared from
`client.DatabricksClient` (originated in Databricks Terraform Provider
back in the early 2020):
- added native integration with `golang.org/x/oauth2` package for both
using token sources in the request as well as injecting a custom
`*http.Client` the context for token refreshes.
- error mapper is now a pluggable function, default implementation
available
- error retry checking is now pluggable function, default implementation
checks for HTTP 429 and 504
- transient error messages are now configurable per client instance
- HTTP request visitors are now configurable per client instance
- Explicit and coupled `Do(ctx context.Context, method, path string,
headers map[string]string, request, response any, visitors
...func(*http.Request) error) error` became concise and flexible `func
(c *ApiClient) Do(ctx context.Context, method, path string, opts
...DoOption) error`. Headers can now be specified with
`httpclient.WithHeaders(headers)`, JSON deserialisation can now be
specified with `httpclient.WithUnmarshal(&entity)`.
- added `httpclient.WithCaptureHeader(key, &value)` to assign headers to
a string pointer. This is required to get a proper integration with
Azure Container Registry Managed Identity passwordless setup.
- body can be specified as `httpclient.WithBody(any)`, that internally
sets a special data field. Technically, this could have been a request
visitor, but we have special logic for "debug bytes" logging, which is
out of scope of this PR change.
- removed flawed trailing double-forward-slash trimming (`//`), as it
was breaking requests containing full URLs.

apierr/errors.go
@@ -154,7 +154,7 @@ func GenericIOError(ue *url.Error) *APIError {
 }
 
 // GetAPIError inspects HTTP errors from the Databricks API for known transient errors.
-func GetAPIError(ctx context.Context, resp *http.Response, body io.ReadCloser) *APIError {
+func GetAPIError(ctx context.Context, resp *http.Response, body io.ReadCloser) error {
 	if resp.StatusCode == 429 {
 		return TooManyRequests()
 	}

client/client.go
@@ -1,29 +1,17 @@
 package client
 
 import (
-	"bytes"
 	"context"
-	"crypto/tls"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net"
 	"net/http"
 	"net/url"
-	"reflect"
-	"runtime"
-	"sort"
-	"strings"
 	"time"
 
 	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/config"
-	"github.com/databricks/databricks-sdk-go/logger"
-	"github.com/databricks/databricks-sdk-go/retries"
+	"github.com/databricks/databricks-sdk-go/httpclient"
 	"github.com/databricks/databricks-sdk-go/useragent"
-	"github.com/google/go-querystring/query"
-	"golang.org/x/time/rate"
 )
 
 func New(cfg *config.Config) (*DatabricksClient, error) {
@@ -33,140 +21,74 @@ func New(cfg *config.Config) (*DatabricksClient, error) {
 	}
 	retryTimeout := time.Duration(orDefault(cfg.RetryTimeoutSeconds, 300)) * time.Second
 	httpTimeout := time.Duration(orDefault(cfg.HTTPTimeoutSeconds, 60)) * time.Second
-	rateLimiter := rate.NewLimiter(rate.Limit(orDefault(cfg.RateLimitPerSecond, 15)), 1)
-	debugTruncateBytes := orDefault(cfg.DebugTruncateBytes, 96)
-	httpTransport := cfg.HTTPTransport
-	if httpTransport == nil {
-		httpTransport = &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).DialContext,
-			ForceAttemptHTTP2:     true,
-			MaxIdleConns:          100,
-			MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
-			IdleConnTimeout:       180 * time.Second,
-			TLSHandshakeTimeout:   30 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: cfg.InsecureSkipVerify,
-			},
-		}
-	}
 	return &DatabricksClient{
-		Config:             cfg,
-		debugHeaders:       cfg.DebugHeaders,
-		debugTruncateBytes: debugTruncateBytes,
-		retryTimeout:       retryTimeout,
-		rateLimiter:        rateLimiter,
-		httpClient: &http.Client{
-			Timeout:   httpTimeout,
-			Transport: httpTransport,
-		},
+		Config: cfg,
+		client: httpclient.NewApiClient(httpclient.ClientConfig{
+			RetryTimeout:       retryTimeout,
+			HTTPTimeout:        httpTimeout,
+			RateLimitPerSecond: orDefault(cfg.RateLimitPerSecond, 15),
+			DebugHeaders:       cfg.DebugHeaders,
+			DebugTruncateBytes: cfg.DebugTruncateBytes,
+			InsecureSkipVerify: cfg.InsecureSkipVerify,
+			Transport:          cfg.HTTPTransport,
+			Visitors: []httpclient.RequestVisitor{
+				cfg.Authenticate,
+				func(r *http.Request) error {
+					if r.URL == nil {
+						return fmt.Errorf("no URL found in request")
+					}
+					url, err := url.Parse(cfg.Host)
+					if err != nil {
+						return err
+					}
+					r.URL.Host = url.Host
+					r.URL.Scheme = url.Scheme
+					return nil
+				},
+				func(r *http.Request) error {
+					ctx := useragent.InContext(r.Context(), "auth", cfg.AuthType)
+					*r = *r.WithContext(ctx) // replace request
+					return nil
+				},
+				func(r *http.Request) error {
+					// Detect if we are running in a CI/CD environment
+					provider := useragent.CiCdProvider()
+					if provider == "" {
+						return nil
+					}
+					// Add the detected CI/CD provider to the user agent
+					ctx := useragent.InContext(r.Context(), "cicd", provider)
+					*r = *r.WithContext(ctx) // replace request
+					return nil
+				},
+			},
+			TransientErrors: []string{
+				"com.databricks.backend.manager.util.UnknownWorkerEnvironmentException",
+				"does not have any associated worker environments",
+				"There is no worker environment with id",
+				"Unknown worker environment",
+				"ClusterNotReadyException",
+				"connection reset by peer",
+				"TLS handshake timeout",
+				"connection refused",
+				"Unexpected error",
+				"i/o timeout",
+			},
+			ErrorMapper: apierr.GetAPIError,
+			ErrorRetriable: func(ctx context.Context, err error) bool {
+				var apiErr *apierr.APIError
+				if errors.As(err, &apiErr) {
+					return apiErr.IsRetriable(ctx)
+				}
+				return false
+			},
+		}),
 	}, nil
 }
 
-type httpClient interface {
-	Do(req *http.Request) (*http.Response, error)
-	CloseIdleConnections()
-}
-
-// Represents a request body.
-//
-// If the provided request data is an io.Reader, DebugBytes is set to
-// "<io.Reader>". Otherwise, DebugBytes is set to the marshaled JSON
-// representation of the request data, and ReadCloser is set to a new
-// io.ReadCloser that reads from DebugBytes.
-//
-// Request bodies are never closed by the client, hence only accepting
-// io.Reader.
-type requestBody struct {
-	Reader     io.Reader
-	DebugBytes []byte
-}
-
-func newRequestBody(data any) (requestBody, error) {
-	switch v := data.(type) {
-	case io.Reader:
-		return requestBody{
-			Reader:     v,
-			DebugBytes: []byte("<io.Reader>"),
-		}, nil
-	case string:
-		return requestBody{
-			Reader:     strings.NewReader(v),
-			DebugBytes: []byte(v),
-		}, nil
-	case []byte:
-		return requestBody{
-			Reader:     bytes.NewReader(v),
-			DebugBytes: v,
-		}, nil
-	default:
-		bs, err := json.Marshal(data)
-		if err != nil {
-			return requestBody{}, fmt.Errorf("request marshal failure: %w", err)
-		}
-		return requestBody{
-			Reader:     bytes.NewReader(bs),
-			DebugBytes: bs,
-		}, nil
-	}
-}
-
-// Reset a request body to its initial state.
-//
-// This is used to retry requests with a body that has already been read.
-// If the request body is not resettable (i.e. not nil and of type other than
-// strings.Reader or bytes.Reader), this will return an error.
-func (r requestBody) reset() error {
-	if r.Reader == nil {
-		return nil
-	}
-	if v, ok := r.Reader.(io.Seeker); ok {
-		_, err := v.Seek(0, io.SeekStart)
-		return err
-	} else {
-		return fmt.Errorf("cannot reset reader of type %T", r.Reader)
-	}
-}
-
-// Represents a response body.
-//
-// Responses must always be closed. For non-streaming responses, they are closed
-// during deserialization in the client (see unmarshall()). For streaming
-// responses, they are returned to the caller, who is responsible for closing
-// them.
-type responseBody struct {
-	ReadCloser io.ReadCloser
-	DebugBytes []byte
-}
-
-func newResponseBody(data any) (responseBody, error) {
-	switch v := data.(type) {
-	case io.ReadCloser:
-		return responseBody{
-			ReadCloser: v,
-			DebugBytes: []byte("<io.ReadCloser>"),
-		}, nil
-	case []byte:
-		return responseBody{
-			ReadCloser: io.NopCloser(bytes.NewReader(v)),
-			DebugBytes: v,
-		}, nil
-	default:
-		return responseBody{}, errors.New("newResponseBody can only be called with io.ReadCloser or []byte")
-	}
-}
-
 type DatabricksClient struct {
-	Config             *config.Config
-	rateLimiter        *rate.Limiter
-	retryTimeout       time.Duration
-	httpClient         httpClient
-	debugHeaders       bool
-	debugTruncateBytes int
+	Config *config.Config
+	client *httpclient.ApiClient
 }
 
 // ConfiguredAccountID returns Databricks Account ID if it's provided in config,
@@ -179,320 +101,14 @@ func (c *DatabricksClient) ConfiguredAccountID() string {
 func (c *DatabricksClient) Do(ctx context.Context, method, path string,
 	headers map[string]string, request, response any,
 	visitors ...func(*http.Request) error) error {
-	body, err := c.perform(ctx, method, path, headers, request, visitors...)
-	if err != nil {
-		return err
+	opts := []httpclient.DoOption{}
+	for _, v := range visitors {
+		opts = append(opts, httpclient.WithRequestVisitor(v))
 	}
-	return c.unmarshal(body, response)
-}
-
-func (c *DatabricksClient) unmarshal(body *responseBody, response any) error {
-	if response == nil {
-		return nil
-	}
-	// If the destination is bytes.Buffer, write the body over there
-	if raw, ok := response.(*io.ReadCloser); ok {
-		*raw = body.ReadCloser
-		return nil
-	}
-	defer body.ReadCloser.Close()
-	bs, err := io.ReadAll(body.ReadCloser)
-	if err != nil {
-		return fmt.Errorf("failed to read response body: %w", err)
-	}
-	if len(bs) == 0 {
-		return nil
-	}
-	// If the destination is a byte slice or buffer, pass the body verbatim.
-	if raw, ok := response.(*[]byte); ok {
-		*raw = bs
-		return nil
-	}
-	if raw, ok := response.(*bytes.Buffer); ok {
-		_, err := raw.Write(bs)
-		return err
-	}
-	return json.Unmarshal(bs, &response)
-}
-
-func (c *DatabricksClient) addHostToRequestUrl(r *http.Request) error {
-	if r.URL == nil {
-		return fmt.Errorf("no URL found in request")
-	}
-	url, err := url.Parse(c.Config.Host)
-	if err != nil {
-		return err
-	}
-	r.URL.Host = url.Host
-	r.URL.Scheme = url.Scheme
-	return nil
-}
-
-func (c *DatabricksClient) fromResponse(r *http.Response) (responseBody, error) {
-	if r == nil {
-		return responseBody{}, fmt.Errorf("nil response")
-	}
-	if r.Request == nil {
-		return responseBody{}, fmt.Errorf("nil request")
-	}
-	streamResponse := r.Request.Header.Get("Accept") != "application/json" && r.Header.Get("Content-Type") != "application/json"
-	if streamResponse {
-		return newResponseBody(r.Body)
-	}
-	defer r.Body.Close()
-	bs, err := io.ReadAll(r.Body)
-	if err != nil {
-		return responseBody{}, fmt.Errorf("response body: %w", err)
-	}
-	return newResponseBody(bs)
-}
-
-func (c *DatabricksClient) redactedDump(prefix string, body []byte) (res string) {
-	return bodyLogger{
-		debugTruncateBytes: c.debugTruncateBytes,
-	}.redactedDump(prefix, body)
-}
-
-// Common error-handling logic for all responses that may need to be retried.
-//
-// If the error is retriable, return a retries.Err to retry the request. However, as the request body will have been consumed
-// by the first attempt, the body must be reset before retrying. If the body cannot be reset, return a retries.Err to halt.
-//
-// Always returns nil for the first parameter as there is no meaningful response body to return in the error case.
-//
-// If it is certain that an error should not be retried, use failRequest() instead.
-func (c *DatabricksClient) handleError(ctx context.Context, err *apierr.APIError, body requestBody) (*responseBody, *retries.Err) {
-	if !err.IsRetriable(ctx) {
-		return c.failRequest(ctx, "non-retriable error", err)
-	}
-	if resetErr := body.reset(); resetErr != nil {
-		return nil, retries.Halt(resetErr)
-	}
-	return nil, retries.Continue(err)
-}
-
-// Fails the request with a retries.Err to halt future retries.
-func (c *DatabricksClient) failRequest(ctx context.Context, msg string, err error) (*responseBody, *retries.Err) {
-	logger.Debugf(ctx, "%s: %s", msg, err)
-	return nil, retries.Halt(err)
-}
-
-func (c *DatabricksClient) attempt(
-	ctx context.Context,
-	method string,
-	requestURL string,
-	headers map[string]string,
-	requestBody requestBody,
-	visitors ...func(*http.Request) error,
-) func() (*responseBody, *retries.Err) {
-	return func() (*responseBody, *retries.Err) {
-		err := c.rateLimiter.Wait(ctx)
-		if err != nil {
-			return c.failRequest(ctx, "failed in rate limiter", err)
-		}
-		request, err := http.NewRequestWithContext(ctx, method, requestURL, requestBody.Reader)
-		if err != nil {
-			return c.failRequest(ctx, "failed creating new request", err)
-		}
-		for k, v := range headers {
-			request.Header.Set(k, v)
-		}
-		for _, requestVisitor := range visitors {
-			err = requestVisitor(request)
-			if err != nil {
-				return c.failRequest(ctx, "failed during request visitor", err)
-			}
-		}
-		// request.Context() holds context potentially enhanced by visitors
-		request.Header.Set("User-Agent", useragent.FromContext(request.Context()))
-
-		// attempt the actual request
-		response, err := c.httpClient.Do(request)
-
-		// After this point, the request body has (probably) been consumed. handleError() must be called to reset it if
-		// possible.
-		if ue, ok := err.(*url.Error); ok {
-			return c.handleError(ctx, apierr.GenericIOError(ue), requestBody)
-		}
-
-		// By this point, the request body has certainly been consumed.
-		responseBody, responseBodyErr := c.fromResponse(response)
-		if responseBodyErr != nil {
-			return c.failRequest(ctx, "failed while reading response", apierr.ReadError(response.StatusCode, responseBodyErr))
-		}
-
-		apiErr := apierr.GetAPIError(ctx, response, responseBody.ReadCloser)
-		defer c.recordRequestLog(ctx, request, response, apiErr, requestBody.DebugBytes, responseBody.DebugBytes)
-
-		if apiErr == nil {
-			return &responseBody, nil
-		}
-
-		// proactively release the connections in HTTP connection pool
-		c.httpClient.CloseIdleConnections()
-		return c.handleError(ctx, apiErr, requestBody)
-	}
-}
-
-func (c *DatabricksClient) recordRequestLog(
-	ctx context.Context,
-	request *http.Request,
-	response *http.Response,
-	err error,
-	requestBody, responseBody []byte,
-) {
-	// Don't compute expensive debug message if debug logging is not enabled.
-	if !logger.Get(ctx).Enabled(ctx, logger.LevelDebug) {
-		return
-	}
-	sb := strings.Builder{}
-	sb.WriteString(fmt.Sprintf("%s %s", request.Method,
-		escapeNewLines(request.URL.Path)))
-	if request.URL.RawQuery != "" {
-		sb.WriteString("?")
-		q, _ := url.QueryUnescape(request.URL.RawQuery)
-		sb.WriteString(q)
-	}
-	sb.WriteString("\n")
-	if c.debugHeaders {
-		if c.Config.Host != "" {
-			sb.WriteString("> * Host: ")
-			sb.WriteString(escapeNewLines(c.Config.Host))
-			sb.WriteString("\n")
-		}
-		for k, v := range request.Header {
-			trunc := onlyNBytes(strings.Join(v, ""), c.debugTruncateBytes)
-			sb.WriteString(fmt.Sprintf("> * %s: %s\n", k, escapeNewLines(trunc)))
-		}
-	}
-	if len(requestBody) > 0 {
-		sb.WriteString(c.redactedDump("> ", requestBody))
-		sb.WriteString("\n")
-	}
-	sb.WriteString("< ")
-	if response != nil {
-		sb.WriteString(fmt.Sprintf("%s %s", response.Proto, response.Status))
-		// Only display error on this line if the response body is empty.
-		// Otherwise the response body will include details about the error.
-		if len(responseBody) == 0 && err != nil {
-			sb.WriteString(fmt.Sprintf(" (Error: %s)", err))
-		}
-	} else {
-		sb.WriteString(fmt.Sprintf("Error: %s", err))
-	}
-	sb.WriteString("\n")
-	if len(responseBody) > 0 {
-		sb.WriteString(c.redactedDump("< ", responseBody))
-	}
-	logger.Debugf(ctx, sb.String()) // lgtm [go/log-injection] lgtm [go/clear-text-logging]
-}
-
-func (c *DatabricksClient) addAuthHeaderToUserAgent(r *http.Request) error {
-	ctx := useragent.InContext(r.Context(), "auth", c.Config.AuthType)
-	*r = *r.WithContext(ctx) // replace request
-	return nil
-}
-
-func (c *DatabricksClient) addCiCdProviderToUserAgent(r *http.Request) error {
-	// Detect if we are running in a CI/CD environment
-	provider := useragent.CiCdProvider()
-	if provider == "" {
-		return nil
-	}
-
-	// Add the detected CI/CD provider to the user agent
-	ctx := useragent.InContext(r.Context(), "cicd", provider)
-	*r = *r.WithContext(ctx) // replace request
-	return nil
-}
-
-func (c *DatabricksClient) perform(
-	ctx context.Context,
-	method,
-	requestURL string,
-	headers map[string]string,
-	data interface{},
-	visitors ...func(*http.Request) error,
-) (*responseBody, error) {
-	// replace double slash in the request URL with a single slash
-	requestURL = strings.Replace(requestURL, "//", "/", -1)
-	requestBody, err := makeRequestBody(method, &requestURL, data)
-	if err != nil {
-		return nil, fmt.Errorf("request marshal: %w", err)
-	}
-	visitors = append([]func(*http.Request) error{
-		c.Config.Authenticate,
-		c.addHostToRequestUrl,
-		c.addAuthHeaderToUserAgent,
-		c.addCiCdProviderToUserAgent,
-	}, visitors...)
-	resp, err := retries.Poll(ctx, c.retryTimeout,
-		c.attempt(ctx, method, requestURL, headers, requestBody, visitors...))
-	if err != nil {
-		// Don't re-wrap, as upper layers may depend on handling apierr.APIError.
-		return nil, err
-	}
-	return resp, nil
-}
-
-func makeQueryString(data interface{}) (string, error) {
-	inputVal := reflect.ValueOf(data)
-	inputType := reflect.TypeOf(data)
-	if inputType.Kind() == reflect.Map {
-		s := []string{}
-		keys := inputVal.MapKeys()
-		// sort map keys by their string repr, so that tests can be deterministic
-		sort.Slice(keys, func(i, j int) bool {
-			return keys[i].String() < keys[j].String()
-		})
-		for _, k := range keys {
-			v := inputVal.MapIndex(k)
-			if v.IsZero() {
-				continue
-			}
-			s = append(s, fmt.Sprintf("%s=%s",
-				strings.Replace(url.QueryEscape(fmt.Sprintf("%v", k.Interface())), "+", "%20", -1),
-				strings.Replace(url.QueryEscape(fmt.Sprintf("%v", v.Interface())), "+", "%20", -1)))
-		}
-		return "?" + strings.Join(s, "&"), nil
-	}
-	if inputType.Kind() == reflect.Struct {
-		params, err := query.Values(data)
-		if err != nil {
-			return "", fmt.Errorf("cannot create query string: %w", err)
-		}
-		// Query parameters may be nested, but the keys generated by
-		// query.Values use the "[" and "]" characters to represent nesting.
-		// Replace all instances of "[" with "." and "]" with empty string
-		// to make the query string compatible with the proto API.
-		// See the following for more information:
-		// https://cloud.google.com/endpoints/docs/grpc-service-config/reference/rpc/google.api#google.api.HttpRule
-		protoCompatibleParams := make(url.Values)
-		for k, vs := range params {
-			newK := strings.Replace(k, "[", ".", -1)
-			newK = strings.Replace(newK, "]", "", -1)
-			for _, v := range vs {
-				protoCompatibleParams.Add(newK, v)
-			}
-		}
-		return "?" + protoCompatibleParams.Encode(), nil
-	}
-	return "", fmt.Errorf("unsupported query string data: %#v", data)
-}
-
-func makeRequestBody(method string, requestURL *string, data interface{}) (requestBody, error) {
-	if data == nil {
-		return requestBody{}, nil
-	}
-	if method == "GET" || method == "DELETE" {
-		qs, err := makeQueryString(data)
-		if err != nil {
-			return requestBody{}, err
-		}
-		*requestURL += qs
-		return newRequestBody([]byte{})
-	}
-	return newRequestBody(data)
+	opts = append(opts, httpclient.WithRequestHeaders(headers))
+	opts = append(opts, httpclient.WithRequestData(request))
+	opts = append(opts, httpclient.WithResponseUnmarshal(response))
+	return c.client.Do(ctx, method, path, opts...)
 }
 
 func orDefault(configured, _default int) int {
@@ -500,11 +116,4 @@ func orDefault(configured, _default int) int {
 		return _default
 	}
 	return configured
-}
-
-// CWE-117 prevention
-func escapeNewLines(in string) string {
-	in = strings.Replace(in, "\n", "", -1)
-	in = strings.Replace(in, "\r", "", -1)
-	return in
 }

commit 89952ab5233966d28cfc75cf4e39e2db3a12a413

Wrap url.Error in APIError (#722)
## Changes

https://github.com/databricks/databricks-sdk-go/commit/b0f3c83d9f5eca12dbd4c1daa03e06aff98a94ff
introduced a regression in the handling of errors returned from
HttpClient.Do. Previously, only some errors returned by Client.Do() were
retried; now, all are retried. This PR adds default error handling logic
in httpclient for errors returned by Client.Do() matching the previous
behavior. As part of this, we remove the duplicated transient error
messages between apierr and the default client.

## Tests
<!-- 
How is this tested? Please see the checklist below and also describe any
other relevant tests
-->

- [ ] `make test` passing
- [ ] `make fmt` applied
- [ ] relevant integration tests applied

apierr/errors.go
@@ -21,11 +21,6 @@ var (
 		"There is no worker environment with id",
 		"Unknown worker environment",
 		"ClusterNotReadyException",
-		"connection reset by peer",
-		"TLS handshake timeout",
-		"connection refused",
-		"Unexpected error",
-		"i/o timeout",
 	}
 )
 

client/client.go
@@ -65,16 +65,6 @@ func New(cfg *config.Config) (*DatabricksClient, error) {
 			},
 			TransientErrors: []string{
 				"REQUEST_LIMIT_EXCEEDED", // This is temporary workaround for SCIM API returning 500.  Remove when it's fixed
-				"com.databricks.backend.manager.util.UnknownWorkerEnvironmentException",
-				"does not have any associated worker environments",
-				"There is no worker environment with id",
-				"Unknown worker environment",
-				"ClusterNotReadyException",
-				"connection reset by peer",
-				"TLS handshake timeout",
-				"connection refused",
-				"Unexpected error",
-				"i/o timeout",
 			},
 			ErrorMapper: apierr.GetAPIError,
 			ErrorRetriable: func(ctx context.Context, err error) bool {

httpclient/api_client.go
@@ -178,8 +178,7 @@ func (c *ApiClient) isRetriable(ctx context.Context, err error) bool {
 	}); ok {
 		skipRetryOnIO = skippable.SkipRetryOnIO()
 	}
-	_, isIO := err.(*url.Error)
-	if isIO && !skipRetryOnIO {
+	if isRetriableUrlError(err) && !skipRetryOnIO {
 		// all IO errors are retriable
 		logger.Debugf(ctx, "Attempting retry because of IO error: %s", err)
 		return true

httpclient/errors.go
@@ -2,9 +2,12 @@ package httpclient
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type HttpError struct {
@@ -46,6 +49,27 @@ func DefaultErrorRetriable(ctx context.Context, err error) bool {
 			return true
 		}
 		if some.StatusCode == 504 {
+			return true
+		}
+	}
+	return false
+}
+
+var urlErrorTransientErrorMessages = []string{
+	"connection reset by peer",
+	"TLS handshake timeout",
+	"connection refused",
+	"Unexpected error",
+	"i/o timeout",
+}
+
+func isRetriableUrlError(err error) bool {
+	var urlError *url.Error
+	if !errors.As(err, &urlError) {
+		return false
+	}
+	for _, msg := range urlErrorTransientErrorMessages {
+		if strings.Contains(err.Error(), msg) {
 			return true
 		}
 	}

httpclient/api_client_test.go
@@ -140,6 +140,41 @@ func TestSimpleRequestRetried(t *testing.T) {
 	require.True(t, retried[0], "request was not retried")
 }
 
+func TestSimpleRequestNotRetried(t *testing.T) {
+	type Dummy struct {
+		Foo int `json:"foo"`
+	}
+	var tried, retried bool
+	transportErr := &url.Error{
+		Op:  "open",
+		URL: "/a/b",
+		Err: fmt.Errorf("some other reason"),
+	}
+
+	c := NewApiClient(ClientConfig{
+		Transport: hc(func(r *http.Request) (*http.Response, error) {
+			if !tried {
+				tried = true
+				return nil, transportErr
+			}
+			retried = true
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(strings.NewReader(`{"foo": 2}`)),
+				Request:    r,
+			}, nil
+		}),
+	})
+	var resp Dummy
+	err := c.Do(context.Background(), "PATCH", "/a",
+		WithRequestData(Dummy{1}),
+		WithResponseUnmarshal(&resp))
+	require.Error(t, err)
+	require.ErrorIs(t, err, transportErr)
+	require.True(t, tried, "request was not tried")
+	require.False(t, retried, "request was retried")
+}
+
 func TestHaltAttemptForLimit(t *testing.T) {
 	ctx := context.Background()
 	c := &ApiClient{