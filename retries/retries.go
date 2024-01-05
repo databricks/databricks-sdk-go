b854d325769b4424c882894aec1201719ae87bfd
Miles Yucht <miles@databricks.com>
2024-01-05 15:07:51 +0100

    Add proper retry abstraction

--- retries/retries.go
+++ retries/retries.go
@@ -2,6 +2,7 @@ package retries
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/rand"
 	"strings"
@@ -41,6 +42,10 @@ type Err struct {
 	Halt bool
 }
 
+func (e *Err) Error() string {
+	return e.Err.Error()
+}
+
 func Halt(err error) *Err {
 	return &Err{err, true}
 }
@@ -57,8 +62,6 @@ func Continuef(format string, err error, args ...interface{}) *Err {
 	wrapped := fmt.Errorf(format, append([]interface{}{err}, args...))
 	return Continue(wrapped)
 }
-
-type WaitFn func() *Err
 
 var maxWait = 10 * time.Second
 var minJitter = 50 * time.Millisecond
@@ -88,55 +91,82 @@ func (et *ErrTimedOut) Unwrap() error {
 	return et.err
 }
 
-func Wait(pctx context.Context, timeout time.Duration, fn WaitFn) error {
-	ctx, cancel := context.WithTimeout(pctx, timeout)
-	defer cancel()
-	var attempt int
-	var lastErr error
-	for {
-		attempt++
-		res := fn()
-		if res == nil {
-			return nil
-		}
-		if res.Halt {
-			return res.Err
-		}
-		lastErr = res.Err
-		wait := Backoff(attempt)
-		timer := time.NewTimer(wait)
-		logger.Tracef(ctx, "%s. Sleeping %s",
-			strings.TrimSuffix(res.Err.Error(), "."),
-			wait.Round(time.Millisecond))
-		select {
-		// stop when either this or parent context times out
-		case <-ctx.Done():
-			timer.Stop()
-			return &ErrTimedOut{lastErr}
-		case <-timer.C:
+type RetryOption func(*RetryConfig)
+
+type RetryConfig struct {
+	timeout    time.Duration
+	shouldHalt func(error) bool
+}
+
+func WithTimeout(timeout time.Duration) RetryOption {
+	return func(rc *RetryConfig) {
+		rc.timeout = timeout
+	}
+}
+
+func OnErrors(on ...error) RetryOption {
+	return func(rc *RetryConfig) {
+		rc.shouldHalt = func(err error) bool {
+			for _, e := range on {
+				if errors.Is(err, e) {
+					return true
+				}
+			}
+			return false
 		}
 	}
 }
 
-func Poll[T any](pctx context.Context, timeout time.Duration, fn func() (*T, *Err)) (*T, error) {
-	ctx, cancel := context.WithTimeout(pctx, timeout)
+func WithHalt(halt func(error) bool) RetryOption {
+	return func(rc *RetryConfig) {
+		rc.shouldHalt = halt
+	}
+}
+
+type retrier[T any] struct {
+	config RetryConfig
+}
+
+func NewWaiter(configOpts ...func(*RetryConfig)) *retrier[struct{}] {
+	return New[struct{}](configOpts...)
+}
+
+func New[T any](configOpts ...func(*RetryConfig)) *retrier[T] {
+	config := RetryConfig{
+		timeout: 20 * time.Minute,
+	}
+	for _, opt := range configOpts {
+		opt(&config)
+	}
+	return &retrier[T]{config}
+}
+
+func (r *retrier[T]) Wait(ctx context.Context, fn func(ctx context.Context) error) error {
+	_, err := r.Run(ctx, func(ctx context.Context) (*T, error) {
+		return nil, fn(ctx)
+	})
+	return err
+}
+
+func (r *retrier[T]) Run(ctx context.Context, fn func(context.Context) (*T, error)) (*T, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.config.timeout)
 	defer cancel()
 	var attempt int
 	var lastErr error
 	for {
 		attempt++
-		entity, err := fn()
+		entity, err := fn(ctx)
 		if err == nil {
 			return entity, nil
 		}
-		if err.Halt {
-			return nil, err.Err
+		if r.config.shouldHalt != nil && r.config.shouldHalt(err) {
+			return nil, err
 		}
-		lastErr = err.Err
+		lastErr = err
 		wait := Backoff(attempt)
 		timer := time.NewTimer(wait)
 		logger.Tracef(ctx, "%s. Sleeping %s",
-			strings.TrimSuffix(err.Err.Error(), "."),
+			strings.TrimSuffix(err.Error(), "."),
 			wait.Round(time.Millisecond))
 		select {
 		// stop when either this or parent context times out
@@ -146,4 +176,32 @@ func Poll[T any](pctx context.Context, timeout time.Duration, fn func() (*T, *Er
 		case <-timer.C:
 		}
 	}
+}
+
+func isHalt(err error) bool {
+	if err == nil {
+		return true
+	}
+	e := err.(*Err)
+	return e == nil || e.Halt
+}
+
+func Wait(ctx context.Context, timeout time.Duration, fn func() *Err) error {
+	return New[struct{}](WithTimeout(timeout), WithHalt(isHalt)).Wait(ctx, func(_ context.Context) error {
+		err := fn()
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func Poll[T any](ctx context.Context, timeout time.Duration, fn func() (*T, *Err)) (*T, error) {
+	return New[T](WithTimeout(timeout), WithHalt(isHalt)).Run(ctx, func(_ context.Context) (*T, error) {
+		res, err := fn()
+		if err != nil {
+			return res, err
+		}
+		return res, nil
+	})
 }
