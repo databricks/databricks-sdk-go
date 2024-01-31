2782548b7c2a417024239ff91d9a43bcda98ed5e
Miles Yucht <miles@databricks.com>
2024-01-31 16:36:08 +0100

    move example to internal

--- account_functions.go
+++ account_functions.go
@@ -10,6 +10,23 @@ import "github.com/databricks/databricks-sdk-go/service/provisioning"
 // The config used for the workspace is identical to that used for the account,
 // except that the host is set to the workspace host, and the account ID is
 // not set.
+//
+// Example:
+//
+//	a, err := databricks.NewAccountClient()
+//	if err != nil {
+//		panic(err)
+//	}
+//	ctx := context.Background()
+//	workspaces, err := a.Workspaces.List(ctx)
+//	if err != nil {
+//		panic(err)
+//	}
+//	w, err := a.GetWorkspaceClient(workspaces[0])
+//	if err != nil {
+//		panic(err)
+//	}
+//	me, err := w.CurrentUser.Me(ctx)
 func (c *AccountClient) GetWorkspaceClient(w provisioning.Workspace) (*WorkspaceClient, error) {
 	host := c.Config.Environment().DeploymentURL(w.DeploymentName)
 	cfg, err := c.Config.NewWithWorkspaceHost(host)
