bd39b6ff28bde02211ae84b1dc3ea450ea54f68f
Miles Yucht <miles@databricks.com>
2024-02-08 18:04:40 +0100

    comment g

--- internal/workspace_client_test.go
+++ internal/workspace_client_test.go
@@ -9,7 +9,7 @@ import (
 func TestUcAccWorkspaceClient_CurrentWorkspaceId(t *testing.T) {
 	ctx, w := ucwsTest(t)
 	expectedWorkspaceId := MustParseInt64(GetEnvOrSkipTest(t, "THIS_WORKSPACE_ID"))
-	workspaceId, err := w.CurrentWorkspaceId(ctx)
+	workspaceId, err := w.CurrentWorkspaceID(ctx)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedWorkspaceId, workspaceId)
 }
--- workspace_functions.go
+++ workspace_functions.go
@@ -7,9 +7,9 @@ import (
 	"github.com/databricks/databricks-sdk-go/httpclient"
 )
 
-// CurrentWorkspaceId returns the workspace ID of the workspace that this client is
+// CurrentWorkspaceID returns the workspace ID of the workspace that this client is
 // connected to.
-func (w *WorkspaceClient) CurrentWorkspaceId(ctx context.Context) (int64, error) {
+func (w *WorkspaceClient) CurrentWorkspaceID(ctx context.Context) (int64, error) {
 	var workspaceIdStr string
 	err := w.apiClient.Client.Do(ctx, "GET", "/api/2.0/preview/scim/v2/Me", httpclient.WithResponseHeader("X-Databricks-Org-Id", &workspaceIdStr))
 	if err != nil {
