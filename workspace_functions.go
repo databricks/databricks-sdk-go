e56387b8e0a2df5b93cfb86d4a8c09bc10ef0a08
Miles Yucht <miles@databricks.com>
2024-02-09 14:32:44 +0100

    leave client private

--- workspace_functions.go
+++ workspace_functions.go
@@ -11,7 +11,7 @@ import (
 // connected to.
 func (w *WorkspaceClient) CurrentWorkspaceID(ctx context.Context) (int64, error) {
 	var workspaceIdStr string
-	err := w.apiClient.Client.Do(ctx, "GET", "/api/2.0/preview/scim/v2/Me", httpclient.WithResponseHeader("X-Databricks-Org-Id", &workspaceIdStr))
+	err := w.apiClient.Do(ctx, "GET", "/api/2.0/preview/scim/v2/Me", httpclient.WithResponseHeader("X-Databricks-Org-Id", &workspaceIdStr))
 	if err != nil {
 		return 0, err
 	}
