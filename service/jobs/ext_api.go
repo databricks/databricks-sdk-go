2a87c50c9fecfb67cbd4a6b57e6bd81173306fb6
Giorgi Kikolashvili <giorgi.kikolashvili@databricks.com>
2025-01-21 13:21:09 +0100

    RunGet also paginates clusters and parameters

--- service/jobs/ext_api.go
+++ service/jobs/ext_api.go
@@ -14,9 +14,8 @@ func (a *JobsAPI) GetRun(ctx context.Context, request GetRunRequest) (*Run, erro
 	// When querying a ForEach task run, a page token is returned when there are more than 100 iterations. Only a single task is returned, corresponding to the ForEach task itself. Therefore, the client only reads the iterations from the next page and not the tasks.
 	isPaginatingIterations := len(run.Iterations) > 0
 
-	pageToken := run.NextPageToken
-	for pageToken != "" {
-		request.PageToken = pageToken
+	for run.NextPageToken != "" {
+		request.PageToken = run.NextPageToken
 		nextRun, err := a.jobsImpl.GetRun(ctx, request)
 		if err != nil {
 			return nil, err
@@ -27,7 +26,10 @@ func (a *JobsAPI) GetRun(ctx context.Context, request GetRunRequest) (*Run, erro
 		} else {
 			run.Tasks = append(run.Tasks, nextRun.Tasks...)
 		}
-		pageToken = nextRun.NextPageToken
+		run.JobClusters = append(run.JobClusters, nextRun.JobClusters...)
+		run.JobParameters = append(run.JobParameters, nextRun.JobParameters...)
+		run.RepairHistory = append(run.RepairHistory, nextRun.RepairHistory...)
+		run.NextPageToken = nextRun.NextPageToken
 	}
 
 	return run, nil
