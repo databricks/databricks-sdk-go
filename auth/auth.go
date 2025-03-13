cbb05b832408c966012825b7e8be3a39ff811494
Renaud Hartert <renaud.hartert@databricks.com>
2025-03-13 16:08:58 +0100

    Update package comment

--- auth/auth.go
+++ auth/auth.go
@@ -1,7 +1,4 @@
 // Package auth is an internal package that provides authentication utilities.
-//
-// IMPORTANT: This package is not meant to be used directly by consumers of the
-// SDK and is subject to change without notice.
 package auth
 
 import (
