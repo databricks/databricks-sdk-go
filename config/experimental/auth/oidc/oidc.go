8a7607599855dc43955f7fbd56fdd7a21c94bec0
Ubuntu <renaud.hartert@databricks.com>
2025-05-05 13:47:49 +0000

    Minor comment fixes

--- config/experimental/auth/oidc/oidc.go
+++ config/experimental/auth/oidc/oidc.go
@@ -9,15 +9,14 @@ import (
 	"os"
 )
 
-// IDToken is a token that can be exchanged for a an access token.
-// Value is the token string.
+// IDToken represents an OIDC ID token that can be exchanged for a Databricks
+// access token.
 type IDToken struct {
 	Value string
 }
 
 // IDTokenSource is anything that returns an IDToken given an audience.
 type IDTokenSource interface {
-	// Function to get the token
 	IDToken(ctx context.Context, audience string) (*IDToken, error)
 }
 
@@ -26,7 +25,7 @@ type IDTokenSource interface {
 //
 // Example:
 //
-//	   ts := IDTokenSourceFn(func(ctx context.Context) (*IDToken, error) {
+//	   ts := IDTokenSourceFn(func(ctx context.Context, aud string) (*IDToken, error) {
 //			return &IDToken{}, nil
 //	   })
 type IDTokenSourceFn func(ctx context.Context, audience string) (*IDToken, error)
@@ -36,15 +35,15 @@ func (fn IDTokenSourceFn) IDToken(ctx context.Context, audience string) (*IDToke
 }
 
 // NewEnvIDTokenSource returns an IDTokenSource that reads the token from
-// environment variable v.
+// environment variable env.
 //
 // Note that the IDTokenSource does not cache the token and will read the token
-// from environment variable v each time.
-func NewEnvIDTokenSource(v string) IDTokenSource {
+// from environment variable env each time.
+func NewEnvIDTokenSource(env string) IDTokenSource {
 	return IDTokenSourceFn(func(ctx context.Context, _ string) (*IDToken, error) {
-		t := os.Getenv(v)
+		t := os.Getenv(env)
 		if t == "" {
-			return nil, fmt.Errorf("missing env var %q", v)
+			return nil, fmt.Errorf("missing env var %q", env)
 		}
 		return &IDToken{Value: t}, nil
 	})
