databricks/databricks-sdk-go

commit ef3a65c6ee8f0de253ce6f554e6d905d6a5fdc85

Return auth error when auth type is set (#1223)
## What changes are proposed in this pull request?

This PR refactors the default auth flow to surface authentication errors
when an auth type is specified.

One side effect of this change is that errors that were previously
surfaced during the default auth flow are now swallowed and only
accessible via trace log.

The PR also slightly simplify the token source strategy.

## How is this tested?

Unit and integration tests.

config/auth_metadata_service.go
@@ -66,15 +66,15 @@ func (c MetadataServiceCredentials) Configure(ctx context.Context, cfg *Config) 
 		metadataServiceURL: parsedMetadataServiceURL,
 		config:             cfg,
 	}
-	response, err := ms.Get()
+	response, err := ms.Get(ctx)
 	if err != nil {
 		return nil, err
 	}
 	if response == nil {
 		return nil, nil
 	}
-	visitor := refreshableVisitor(&ms)
-	return credentials.NewCredentialsProvider(visitor), nil
+
+	return credentials.NewOAuthCredentialsProviderFromTokenSource(ms), nil
 }
 
 type metadataService struct {
@@ -83,8 +83,8 @@ type metadataService struct {
 }
 
 // performs a request to the metadata service and returns the token
-func (s metadataService) Get() (*oauth2.Token, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), metadataServiceTimeout)
+func (s metadataService) Get(ctx context.Context) (*oauth2.Token, error) {
+	ctx, cancel := context.WithTimeout(ctx, metadataServiceTimeout)
 	defer cancel()
 	var inner msiToken
 	err := s.config.refreshClient.Do(ctx, http.MethodGet,
@@ -99,15 +99,15 @@ func (s metadataService) Get() (*oauth2.Token, error) {
 	return inner.Token()
 }
 
-func (t metadataService) Token() (*oauth2.Token, error) {
-	token, err := t.Get()
+func (t metadataService) Token(ctx context.Context) (*oauth2.Token, error) {
+	token, err := t.Get(ctx)
 	if err != nil {
 		return nil, err
 	}
 	if token == nil {
 		return nil, fmt.Errorf("no token returned from metadata service")
 	}
-	logger.Debugf(context.Background(),
+	logger.Debugf(ctx,
 		"Refreshed access token from local metadata service, which expires on %s",
 		token.Expiry.Format(time.RFC3339))
 	return token, nil

config/credentials/credentials.go
@@ -1,8 +1,11 @@
 package credentials
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/databricks/databricks-sdk-go/config/experimental/auth"
 	"golang.org/x/oauth2"
 )
 
@@ -32,6 +35,41 @@ type OAuthCredentialsProvider interface {
 	Token() (*oauth2.Token, error)
 }
 
+// NewOAuthCredentialsProviderFromTokenSource returns a new OAuthCredentialsProvider
+// that uses the given TokenSource to get the token.
+//
+// The returned OAuthCredentialsProvider does not alter the behavior of the token
+// source. For example, it does not implement any caching or retrying logic. It
+// is the responsibility of the TokenSource to implement these behaviors.
+func NewOAuthCredentialsProviderFromTokenSource(ts auth.TokenSource) OAuthCredentialsProvider {
+	return &tsOAuthCredentialsProvider{ts}
+}
+
+type tsOAuthCredentialsProvider struct {
+	ts auth.TokenSource
+}
+
+func (cp tsOAuthCredentialsProvider) SetHeaders(r *http.Request) error {
+	token, err := cp.ts.Token(context.Background())
+	if err != nil {
+		return fmt.Errorf("error getting token: %w", err)
+	}
+	token.SetAuthHeader(r)
+	return nil
+}
+
+func (cp tsOAuthCredentialsProvider) Token() (*oauth2.Token, error) {
+	return cp.ts.Token(context.Background())
+}
+
+// DEPRECATED: Use NewOAuthCredentialsProviderFromTokenSource instead.
+func NewOAuthCredentialsProvider(visitor func(r *http.Request) error, tokenProvider func() (*oauth2.Token, error)) OAuthCredentialsProvider {
+	return &oauthCredentialsProvider{
+		setHeaders: visitor,
+		token:      tokenProvider,
+	}
+}
+
 type oauthCredentialsProvider struct {
 	setHeaders func(r *http.Request) error
 	token      func() (*oauth2.Token, error)
@@ -43,11 +81,4 @@ func (c *oauthCredentialsProvider) SetHeaders(r *http.Request) error {
 
 func (c *oauthCredentialsProvider) Token() (*oauth2.Token, error) {
 	return c.token()
-}
-
-func NewOAuthCredentialsProvider(visitor func(r *http.Request) error, tokenProvider func() (*oauth2.Token, error)) OAuthCredentialsProvider {
-	return &oauthCredentialsProvider{
-		setHeaders: visitor,
-		token:      tokenProvider,
-	}
 }

config/token_source_strategy.go
@@ -2,46 +2,36 @@ package config
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/databricks/databricks-sdk-go/config/credentials"
 	"github.com/databricks/databricks-sdk-go/config/experimental/auth"
-	"github.com/databricks/databricks-sdk-go/config/experimental/auth/authconv"
-	"github.com/databricks/databricks-sdk-go/logger"
 )
 
 // Creates a CredentialsStrategy from a TokenSource.
-func NewTokenSourceStrategy(
-	name string,
-	tokenSource auth.TokenSource,
-) CredentialsStrategy {
-	return &tokenSourceStrategy{
-		name:        name,
-		tokenSource: tokenSource,
-	}
+func NewTokenSourceStrategy(name string, ts auth.TokenSource) CredentialsStrategy {
+	return &tokenSourceStrategy{name: name, ts: ts}
 }
 
-// tokenSourceStrategy is wrapper on a auth.TokenSource which converts it into a CredentialsStrategy
+// tokenSourceStrategy is wrapper on a auth.TokenSource which converts it into
+// a CredentialsStrategy.
 type tokenSourceStrategy struct {
-	tokenSource auth.TokenSource
-	name        string
+	name string
+	ts   auth.TokenSource
 }
 
 // Configure implements [CredentialsStrategy.Configure].
-func (t *tokenSourceStrategy) Configure(ctx context.Context, cfg *Config) (credentials.CredentialsProvider, error) {
+func (tss *tokenSourceStrategy) Configure(ctx context.Context, cfg *Config) (credentials.CredentialsProvider, error) {
+	cp := credentials.NewOAuthCredentialsProviderFromTokenSource(auth.NewCachedTokenSource(tss.ts))
 
-	// If we cannot get a token, skip this CredentialsStrategy.
-	// We don't want to fail here because it's possible that the supplier is enabled
-	// without the user action. For instance, jobs running in GitHub will have
-	// OIDC environment variables added automatically
-	cached := auth.NewCachedTokenSource(t.tokenSource)
-	if _, err := cached.Token(ctx); err != nil {
-		logger.Debugf(ctx, fmt.Sprintf("Skipping %s due to error: %v", t.name, err))
-		return nil, nil
+	// Sanity check that a token can be obtained.
+	//
+	// TODO: Move this outside of this function. If credentials providers have
+	// to be tested, this should be done in the main default loop, not here.
+	if _, err := cp.Token(); err != nil {
+		return nil, err
 	}
 
-	visitor := refreshableAuthVisitor(cached)
-	return credentials.NewOAuthCredentialsProvider(visitor, authconv.OAuth2TokenSource(cached).Token), nil
+	return cp, nil
 }
 
 // Name implements [CredentialsStrategy.Name].

config/auth_m2m_test.go
@@ -74,6 +74,7 @@ func TestM2mNotSupported(t *testing.T) {
 		Host:         "a",
 		ClientID:     "b",
 		ClientSecret: "c",
+		AuthType:     "oauth-m2m",
 		HTTPTransport: fixtures.MappingTransport{
 			"GET /oidc/.well-known/oauth-authorization-server": {
 				Status:   404,

config/auth_azure_msi_test.go
@@ -47,6 +47,7 @@ func TestMsiHappyFlow(t *testing.T) {
 	assertHeaders(t, &Config{
 		AzureUseMSI:     true,
 		AzureResourceID: "/a/b/c",
+		AuthType:        "azure-msi",
 		HTTPTransport: fixtures.MappingTransport{
 			"GET /metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F": {
 				ExpectedHeaders: map[string]string{
@@ -86,6 +87,7 @@ func TestMsiFailsOnResolveWorkspace(t *testing.T) {
 	_, err := authenticateRequest(&Config{
 		AzureUseMSI:     true,
 		AzureResourceID: "/a/b/c",
+		AuthType:        "azure-msi",
 		HTTPTransport: fixtures.MappingTransport{
 			"GET /metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F": {
 				Response: someValidToken("bcd"),
@@ -108,6 +110,7 @@ func TestMsiTokenNotFound(t *testing.T) {
 		AzureUseMSI:     true,
 		AzureClientID:   "abc",
 		AzureResourceID: "/a/b/c",
+		AuthType:        "azure-msi",
 		HTTPTransport: fixtures.MappingTransport{
 			"GET /metadata/identity/oauth2/token?api-version=2018-02-01&client_id=abc&resource=https%3A%2F%2Fmanagement.azure.com%2F": {
 				Status:   404,
@@ -122,6 +125,7 @@ func TestMsiInvalidTokenExpiry(t *testing.T) {
 	_, err := authenticateRequest(&Config{
 		AzureUseMSI:     true,
 		AzureResourceID: "/a/b/c",
+		AuthType:        "azure-msi",
 		HTTPTransport: fixtures.MappingTransport{
 			"GET /metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F": {
 				Response: map[string]any{

commit c54b388c90dd97b889e1e1ce9161d1e32b640dbb

Fix caching issue in the `metadata-service` auth type. (#1225)
## What changes are proposed in this pull request?

This PR fixes a bug introduced in PR #1223 which led token to not be
cached anymore when using the `metadata-service` auth type. The PR also
slightly cleans up some authentication code by consolidating on using
Databricks' version of the TokenSource.

## How is this tested?

Unit and integration tests.

config/api_client.go
@@ -106,5 +106,5 @@ type noopAuth struct{}
 func (noopAuth) Name() string { return "noop" }
 func (noopAuth) Configure(context.Context, *Config) (credentials.CredentialsProvider, error) {
 	visitor := func(r *http.Request) error { return nil }
-	return credentials.NewCredentialsProvider(visitor), nil
+	return credentials.CredentialsProviderFn(visitor), nil
 }

config/auth_azure_github_oidc.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/databricks/databricks-sdk-go/config/credentials"
+	"github.com/databricks/databricks-sdk-go/config/experimental/auth"
 	"github.com/databricks/databricks-sdk-go/config/experimental/auth/oidc"
 	"github.com/databricks/databricks-sdk-go/httpclient"
 	"golang.org/x/oauth2"
@@ -49,7 +50,8 @@ func (c AzureGithubOIDCCredentials) Configure(ctx context.Context, cfg *Config) 
 		httpClient:    cfg.refreshClient,
 	}
 
-	return credentials.NewOAuthCredentialsProvider(refreshableVisitor(ts), ts.Token), nil
+	cts := auth.NewCachedTokenSource(ts)
+	return credentials.NewOAuthCredentialsProviderFromTokenSource(cts), nil
 }
 
 // azureOIDCTokenSource implements [oauth2.TokenSource] to obtain Azure auth
@@ -65,8 +67,8 @@ type azureOIDCTokenSource struct {
 
 const azureOICDTimeout = 10 * time.Second
 
-func (ts *azureOIDCTokenSource) Token() (*oauth2.Token, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), azureOICDTimeout)
+func (ts *azureOIDCTokenSource) Token(ctx context.Context) (*oauth2.Token, error) {
+	ctx, cancel := context.WithTimeout(ctx, azureOICDTimeout)
 	defer cancel()
 
 	resp := struct { // anonymous struct to parse the response

config/auth_azure_msi.go
@@ -105,7 +105,6 @@ func (token msiToken) Token() (*oauth2.Token, error) {
 	}
 	epoch, err := token.ExpiresOn.Int64()
 	if err != nil {
-		// go 1.19 doesn't support multiple error unwraps
 		return nil, fmt.Errorf("%w: %s", errInvalidTokenExpiry, err)
 	}
 	return &oauth2.Token{

config/auth_basic.go
@@ -26,5 +26,5 @@ func (c BasicCredentials) Configure(ctx context.Context, cfg *Config) (credentia
 		r.Header.Set("Authorization", fmt.Sprintf("Basic %s", b64))
 		return nil
 	}
-	return credentials.NewCredentialsProvider(visitor), nil
+	return credentials.CredentialsProviderFn(visitor), nil
 }

config/auth_gcp_google_id.go
@@ -31,7 +31,7 @@ func (c GoogleDefaultCredentials) Configure(ctx context.Context, cfg *Config) (c
 	if !cfg.IsAccountClient() {
 		logger.Infof(ctx, "Using Google Default Application Credentials for Workspace")
 		visitor := refreshableVisitor(inner)
-		return credentials.NewCredentialsProvider(visitor), nil
+		return credentials.CredentialsProviderFn(visitor), nil
 	}
 	// source for generateAccessToken
 	platform, err := impersonate.CredentialsTokenSource(ctx, impersonate.CredentialsConfig{

config/auth_m2m.go
@@ -11,8 +11,7 @@ import (
 	"github.com/databricks/databricks-sdk-go/logger"
 )
 
-type M2mCredentials struct {
-}
+type M2mCredentials struct{}
 
 func (c M2mCredentials) Name() string {
 	return "oauth-m2m"
@@ -34,6 +33,7 @@ func (c M2mCredentials) Configure(ctx context.Context, cfg *Config) (credentials
 		TokenURL:     endpoints.TokenEndpoint,
 		Scopes:       []string{"all-apis"},
 	}).TokenSource(ctx)
+
 	visitor := refreshableVisitor(ts)
 	return credentials.NewOAuthCredentialsProvider(visitor, ts.Token), nil
 }

config/auth_metadata_service.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/databricks/databricks-sdk-go/config/credentials"
+	"github.com/databricks/databricks-sdk-go/config/experimental/auth"
 	"github.com/databricks/databricks-sdk-go/httpclient"
 	"github.com/databricks/databricks-sdk-go/logger"
 	"golang.org/x/oauth2"
@@ -55,26 +56,29 @@ func (c MetadataServiceCredentials) Configure(ctx context.Context, cfg *Config) 
 	}
 	parsedMetadataServiceURL, err := url.Parse(cfg.MetadataServiceURL)
 	if err != nil {
-		// go 1.19 doesn't allow multiple error unwraping
 		return nil, fmt.Errorf("%w: %s", errMetadataServiceMalformed, err)
 	}
-	// only allow localhost URLs
+
+	// Only allow localhost URLs.
 	if parsedMetadataServiceURL.Hostname() != "localhost" && parsedMetadataServiceURL.Hostname() != "127.0.0.1" {
 		return nil, fmt.Errorf("%w: %s", errMetadataServiceNotLocalhost, cfg.MetadataServiceURL)
 	}
+
 	ms := metadataService{
 		metadataServiceURL: parsedMetadataServiceURL,
 		config:             cfg,
 	}
-	response, err := ms.Get(ctx)
-	if err != nil {
+
+	// Sanity check that a token can be obtained.
+	//
+	// TODO: Move this outside of this function. If credentials providers have
+	// to be tested, this should be done in the main default loop, not here.
+	if _, err := ms.Token(ctx); err != nil {
 		return nil, err
 	}
-	if response == nil {
-		return nil, nil
-	}
 
-	return credentials.NewOAuthCredentialsProviderFromTokenSource(ms), nil
+	cts := auth.NewCachedTokenSource(ms)
+	return credentials.NewOAuthCredentialsProviderFromTokenSource(cts), nil
 }
 
 type metadataService struct {
@@ -82,33 +86,26 @@ type metadataService struct {
 	config             *Config
 }
 
-// performs a request to the metadata service and returns the token
-func (s metadataService) Get(ctx context.Context) (*oauth2.Token, error) {
+func (ms metadataService) Token(ctx context.Context) (*oauth2.Token, error) {
 	ctx, cancel := context.WithTimeout(ctx, metadataServiceTimeout)
 	defer cancel()
-	var inner msiToken
-	err := s.config.refreshClient.Do(ctx, http.MethodGet,
-		s.metadataServiceURL.String(),
+
+	var mt msiToken
+	err := ms.config.refreshClient.Do(ctx, http.MethodGet,
+		ms.metadataServiceURL.String(),
 		httpclient.WithRequestHeader(MetadataServiceVersionHeader, MetadataServiceVersion),
-		httpclient.WithRequestHeader(MetadataServiceHostHeader, s.config.Host),
-		httpclient.WithResponseUnmarshal(&inner),
+		httpclient.WithRequestHeader(MetadataServiceHostHeader, ms.config.Host),
+		httpclient.WithResponseUnmarshal(&mt),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("token request: %w", err)
 	}
-	return inner.Token()
-}
 
-func (t metadataService) Token(ctx context.Context) (*oauth2.Token, error) {
-	token, err := t.Get(ctx)
+	token, err := mt.Token()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("token parse: %w", err)
 	}
-	if token == nil {
-		return nil, fmt.Errorf("no token returned from metadata service")
-	}
-	logger.Debugf(ctx,
-		"Refreshed access token from local metadata service, which expires on %s",
-		token.Expiry.Format(time.RFC3339))
+
+	logger.Debugf(ctx, "Refreshed access token from local metadata service, which expires on %s", token.Expiry.Format(time.RFC3339))
 	return token, nil
 }

config/auth_pat.go
@@ -23,5 +23,5 @@ func (c PatCredentials) Configure(ctx context.Context, cfg *Config) (credentials
 		r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", cfg.Token))
 		return nil
 	}
-	return credentials.NewCredentialsProvider(visitor), nil
+	return credentials.CredentialsProviderFn(visitor), nil
 }

config/config.go
@@ -15,7 +15,6 @@ import (
 	"github.com/databricks/databricks-sdk-go/common/environment"
 	"github.com/databricks/databricks-sdk-go/config/credentials"
 	"github.com/databricks/databricks-sdk-go/config/experimental/auth"
-	"github.com/databricks/databricks-sdk-go/config/experimental/auth/authconv"
 	"github.com/databricks/databricks-sdk-go/credentials/u2m"
 	"github.com/databricks/databricks-sdk-go/httpclient"
 	"github.com/databricks/databricks-sdk-go/logger"
@@ -252,7 +251,7 @@ func (c *Config) GetTokenSource() auth.TokenSource {
 		return errorTokenSource(err)
 	}
 	if h, ok := c.credentialsProvider.(credentials.OAuthCredentialsProvider); ok {
-		return authconv.AuthTokenSource(h)
+		return h
 	} else {
 		return errorTokenSource(fmt.Errorf("OAuth Token not supported for current auth type %s", c.AuthType))
 	}

config/credentials/credentials.go
@@ -16,23 +16,33 @@ type CredentialsProvider interface {
 	SetHeaders(r *http.Request) error
 }
 
-type credentialsProvider func(r *http.Request) error
+// CredentialsProviderFn is an adapter to allow the use of an ordinary function
+// as a CredentialsProvider.
+//
+// Example:
+//
+//	   cp := CredentialsProviderFn(func(r *http.Request) error {
+//			return nil
+//	   })
+type CredentialsProviderFn func(r *http.Request) error
 
-func (c credentialsProvider) SetHeaders(r *http.Request) error {
+func (c CredentialsProviderFn) SetHeaders(r *http.Request) error {
 	return c(r)
 }
 
 // NewCredentialsProvider returns a new CredentialsProvider that uses the
 // provided function to set headers on the request.
+//
+// DEPRECATED: Use CredentialsProviderFn instead.
 func NewCredentialsProvider(f func(r *http.Request) error) CredentialsProvider {
-	return credentialsProvider(f)
+	return CredentialsProviderFn(f)
 }
 
 // OAuthCredentialsProvider is a specialized CredentialsProvider uses and provides an OAuth token.
 type OAuthCredentialsProvider interface {
 	CredentialsProvider
 	// Token returns the OAuth token generated by the provider.
-	Token() (*oauth2.Token, error)
+	Token(ctx context.Context) (*oauth2.Token, error)
 }
 
 // NewOAuthCredentialsProviderFromTokenSource returns a new OAuthCredentialsProvider
@@ -58,8 +68,8 @@ func (cp tsOAuthCredentialsProvider) SetHeaders(r *http.Request) error {
 	return nil
 }
 
-func (cp tsOAuthCredentialsProvider) Token() (*oauth2.Token, error) {
-	return cp.ts.Token(context.Background())
+func (cp tsOAuthCredentialsProvider) Token(ctx context.Context) (*oauth2.Token, error) {
+	return cp.ts.Token(ctx)
 }
 
 // DEPRECATED: Use NewOAuthCredentialsProviderFromTokenSource instead.
@@ -79,6 +89,6 @@ func (c *oauthCredentialsProvider) SetHeaders(r *http.Request) error {
 	return c.setHeaders(r)
 }
 
-func (c *oauthCredentialsProvider) Token() (*oauth2.Token, error) {
+func (c *oauthCredentialsProvider) Token(_ context.Context) (*oauth2.Token, error) {
 	return c.token()
 }

config/token_source_strategy.go
@@ -27,7 +27,7 @@ func (tss *tokenSourceStrategy) Configure(ctx context.Context, cfg *Config) (cre
 	//
 	// TODO: Move this outside of this function. If credentials providers have
 	// to be tested, this should be done in the main default loop, not here.
-	if _, err := cp.Token(); err != nil {
+	if _, err := cp.Token(ctx); err != nil {
 		return nil, err
 	}
 

examples/custom-auth/main.go
@@ -28,7 +28,7 @@ func (c *CustomCredentials) Configure(
 		r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 		return nil
 	}
-	return credentials.NewCredentialsProvider(visitor), nil
+	return credentials.CredentialsProviderFn(visitor), nil
 }
 
 func main() {

config/auth_azure_github_oidc_test.go
@@ -168,7 +168,7 @@ func TestAzureGithubOIDCCredentials(t *testing.T) {
 					},
 				},
 			},
-			wantErrPrefix: errPrefix("inner token: http 500"),
+			wantErrPrefix: errPrefix("error getting token: http 500"),
 		},
 		{
 			desc: "invalid auth token",
@@ -204,7 +204,7 @@ func TestAzureGithubOIDCCredentials(t *testing.T) {
 					},
 				},
 			},
-			wantErrPrefix: errPrefix("inner token: invalid token"),
+			wantErrPrefix: errPrefix("error getting token: invalid token"),
 		},
 		{
 			desc: "success",

config/auth_m2m_test.go
@@ -11,62 +11,70 @@ import (
 )
 
 func TestM2mHappyFlow(t *testing.T) {
-	assertHeaders(t, &Config{
-		Host:         "a",
-		ClientID:     "b",
-		ClientSecret: "c",
-		HTTPTransport: fixtures.MappingTransport{
-			"GET /oidc/.well-known/oauth-authorization-server": {
-				Response: u2m.OAuthAuthorizationServer{
-					AuthorizationEndpoint: "https://localhost:1234/dummy/auth",
-					TokenEndpoint:         "https://localhost:1234/dummy/token",
+	assertHeaders(
+		t,
+		&Config{
+			Host:         "a",
+			ClientID:     "b",
+			ClientSecret: "c",
+			AuthType:     "oauth-m2m",
+			HTTPTransport: fixtures.MappingTransport{
+				"GET /oidc/.well-known/oauth-authorization-server": {
+					Response: u2m.OAuthAuthorizationServer{
+						AuthorizationEndpoint: "https://localhost:1234/dummy/auth",
+						TokenEndpoint:         "https://localhost:1234/dummy/token",
+					},
 				},
-			},
-			"POST /dummy/token": {
-				ExpectedHeaders: map[string]string{
-					"Authorization": "Basic Yjpj",
-					"Content-Type":  "application/x-www-form-urlencoded",
-				},
-				ExpectedRequest: url.Values{
-					"grant_type": {"client_credentials"},
-					"scope":      {"all-apis"},
-				},
-				Response: oauth2.Token{
-					TokenType:   "Some",
-					AccessToken: "cde",
+				"POST /dummy/token": {
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Basic Yjpj",
+						"Content-Type":  "application/x-www-form-urlencoded",
+					},
+					ExpectedRequest: url.Values{
+						"grant_type": {"client_credentials"},
+						"scope":      {"all-apis"},
+					},
+					Response: oauth2.Token{
+						TokenType:   "Some",
+						AccessToken: "cde",
+					},
 				},
 			},
 		},
-	}, map[string]string{
-		"Authorization": "Some cde",
-	})
+		map[string]string{
+			"Authorization": "Some cde",
+		},
+	)
 }
 
 func TestM2mHappyFlowForAccount(t *testing.T) {
-	assertHeaders(t, &Config{
-		Host:         "accounts.cloud.databricks.com",
-		AccountID:    "a",
-		ClientID:     "b",
-		ClientSecret: "c",
-		HTTPTransport: fixtures.MappingTransport{
-			"POST /oidc/accounts/a/v1/token": {
-				ExpectedHeaders: map[string]string{
-					"Authorization": "Basic Yjpj",
-					"Content-Type":  "application/x-www-form-urlencoded",
-				},
-				ExpectedRequest: url.Values{
-					"grant_type": {"client_credentials"},
-					"scope":      {"all-apis"},
-				},
-				Response: oauth2.Token{
-					TokenType:   "Some",
-					AccessToken: "cde",
+	assertHeaders(t,
+		&Config{
+			Host:         "accounts.cloud.databricks.com",
+			AccountID:    "a",
+			ClientID:     "b",
+			ClientSecret: "c",
+			HTTPTransport: fixtures.MappingTransport{
+				"POST /oidc/accounts/a/v1/token": {
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Basic Yjpj",
+						"Content-Type":  "application/x-www-form-urlencoded",
+					},
+					ExpectedRequest: url.Values{
+						"grant_type": {"client_credentials"},
+						"scope":      {"all-apis"},
+					},
+					Response: oauth2.Token{
+						TokenType:   "Some",
+						AccessToken: "cde",
+					},
 				},
 			},
 		},
-	}, map[string]string{
-		"Authorization": "Some cde",
-	})
+		map[string]string{
+			"Authorization": "Some cde",
+		},
+	)
 }
 
 func TestM2mNotSupported(t *testing.T) {