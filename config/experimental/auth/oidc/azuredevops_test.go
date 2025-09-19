47a40f9643c9f7fbb7792b8d7d4556a59f1b8086
Divyansh Vijayvergia <divyansh.vijayvergia@databricks.com>
2025-09-19 16:45:12 +0000

    added unit tests for azure devops OIDC

---
+++ config/experimental/auth/oidc/azuredevops_test.go
@@ -0,0 +1,195 @@
+package oidc
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go/httpclient"
+	"github.com/databricks/databricks-sdk-go/httpclient/fixtures"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAzureDevOpsIDTokenSource(t *testing.T) {
+	testCases := []struct {
+		desc                                   string
+		azureDevOpsAccessToken                 string
+		azureDevOpsTeamFoundationCollectionUri string
+		azureDevOpsPlanId                      string
+		azureDevOpsJobId                       string
+		azureDevOpsTeamProjectId               string
+		azureDevOpsHostType                    string
+		audience                               string
+		httpTransport                          http.RoundTripper
+		wantToken                              *IDToken
+		wantErrPrefix                          *string
+	}{
+		{
+			desc:                                   "missing access token",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			wantErrPrefix:                          errPrefix("missing SYSTEM_ACCESSTOKEN"),
+		},
+		{
+			desc:                     "missing team foundation collection uri",
+			azureDevOpsAccessToken:   "token-1337",
+			azureDevOpsPlanId:        "plan123",
+			azureDevOpsJobId:         "job456",
+			azureDevOpsTeamProjectId: "project789",
+			azureDevOpsHostType:      "build",
+			wantErrPrefix:            errPrefix("missing SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"),
+		},
+		{
+			desc:                                   "missing plan id",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			wantErrPrefix:                          errPrefix("missing SYSTEM_PLANID"),
+		},
+		{
+			desc:                                   "missing job id",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			wantErrPrefix:                          errPrefix("missing SYSTEM_JOBID"),
+		},
+		{
+			desc:                                   "missing team project id",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsHostType:                    "build",
+			wantErrPrefix:                          errPrefix("missing SYSTEM_TEAMPROJECTID"),
+		},
+		{
+			desc:                                   "missing host type",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			wantErrPrefix:                          errPrefix("missing SYSTEM_HOSTTYPE"),
+		},
+		{
+			desc:                                   "error getting token - server error",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			httpTransport: fixtures.MappingTransport{
+				"POST /myorg/project789/_apis/distributedtask/hubs/build/plans/plan123/jobs/job456/oidctoken?api-version=7.2-preview.1": {
+					Status: http.StatusInternalServerError,
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Bearer token-1337",
+						"Accept":        "application/json",
+					},
+				},
+			},
+			wantErrPrefix: errPrefix("failed to request ID token from Azure DevOps"),
+		},
+		{
+			desc:                                   "success with build host type",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			audience:                               "test-audience",
+			httpTransport: fixtures.MappingTransport{
+				"POST /myorg/project789/_apis/distributedtask/hubs/build/plans/plan123/jobs/job456/oidctoken?api-version=7.2-preview.1": {
+					Status: http.StatusOK,
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Bearer token-1337",
+						"Accept":        "application/json",
+					},
+					Response: `{"oidcToken": "azure-devops-id-token-42"}`,
+				},
+			},
+			wantToken: &IDToken{
+				Value: "azure-devops-id-token-42",
+			},
+		},
+		{
+			desc:                                   "success with release host type",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "release",
+			audience:                               "test-audience",
+			httpTransport: fixtures.MappingTransport{
+				"POST /myorg/project789/_apis/distributedtask/hubs/release/plans/plan123/jobs/job456/oidctoken?api-version=7.2-preview.1": {
+					Status: http.StatusOK,
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Bearer token-1337",
+						"Accept":        "application/json",
+					},
+					Response: `{"oidcToken": "azure-devops-release-token-42"}`,
+				},
+			},
+			wantToken: &IDToken{
+				Value: "azure-devops-release-token-42",
+			},
+		},
+		{
+			desc:                                   "success with empty oidc token in response",
+			azureDevOpsAccessToken:                 "token-1337",
+			azureDevOpsTeamFoundationCollectionUri: "https://dev.azure.com/myorg",
+			azureDevOpsPlanId:                      "plan123",
+			azureDevOpsJobId:                       "job456",
+			azureDevOpsTeamProjectId:               "project789",
+			azureDevOpsHostType:                    "build",
+			httpTransport: fixtures.MappingTransport{
+				"POST /myorg/project789/_apis/distributedtask/hubs/build/plans/plan123/jobs/job456/oidctoken?api-version=7.2-preview.1": {
+					Status: http.StatusOK,
+					ExpectedHeaders: map[string]string{
+						"Authorization": "Bearer token-1337",
+						"Accept":        "application/json",
+					},
+					Response: `{"oidcToken": ""}`,
+				},
+			},
+			wantErrPrefix: errPrefix("empty OIDC token received from Azure DevOps"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			cli := httpclient.NewApiClient(httpclient.ClientConfig{
+				Transport: tc.httpTransport,
+			})
+			p := &azureDevOpsIDTokenSource{
+				azureDevOpsAccessToken:                 tc.azureDevOpsAccessToken,
+				azureDevOpsTeamFoundationCollectionUri: tc.azureDevOpsTeamFoundationCollectionUri,
+				azureDevOpsPlanId:                      tc.azureDevOpsPlanId,
+				azureDevOpsJobId:                       tc.azureDevOpsJobId,
+				azureDevOpsTeamProjectId:               tc.azureDevOpsTeamProjectId,
+				azureDevOpsHostType:                    tc.azureDevOpsHostType,
+				refreshClient:                          cli,
+			}
+			token, gotErr := p.IDToken(context.Background(), tc.audience)
+
+			if tc.wantErrPrefix == nil && gotErr != nil {
+				t.Errorf("IDToken(): got error %q, want none", gotErr)
+			}
+			if tc.wantErrPrefix != nil && !hasPrefix(gotErr, *tc.wantErrPrefix) {
+				t.Errorf("IDToken(): got error %q, want error with prefix %q", gotErr, *tc.wantErrPrefix)
+			}
+			if diff := cmp.Diff(tc.wantToken, token); diff != "" {
+				t.Errorf("IDToken(): mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
