592a79dcbafac89954adbd10a1ae543e40265d68
Divyansh Vijayvergia <divyansh.vijayvergia@databricks.com>
2025-10-10 13:21:55 +0000

    deleted orpheded files

--- service/pkg.go
+++ service/pkg.go
@@ -64,9 +64,9 @@
 //
 // - [marketplace.ConsumerProvidersAPI]: Providers are the entities that publish listings to the Marketplace.
 //
-// - [provisioning.CredentialsAPI]: These APIs manage credential configurations for this workspace.
-//
 // - [catalog.CredentialsAPI]: A credential represents an authentication and authorization mechanism for accessing services on your cloud tenant.
+//
+// - [provisioning.CredentialsAPI]: These APIs manage credential configurations for this workspace.
 //
 // - [settings.CredentialsManagerAPI]: Credentials manager interacts with with Identity Providers to to perform token exchanges using stored credentials and refresh tokens.
 //
