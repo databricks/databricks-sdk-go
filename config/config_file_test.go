8aa33e55d273b31937d2c99a891941c999c104dd
tejas-kochar <tkochar20@gmail.com>
2026-01-02 11:19:11 +0000

    add cleanup environment back to test

--- config/config_file_test.go
+++ config/config_file_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"testing"
 
+	"github.com/databricks/databricks-sdk-go/internal/env"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -55,6 +56,7 @@ func TestConfigFile_Scopes(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			env.CleanupEnvironment(t)
 			t.Setenv("HOME", "testdata")
 
 			cfg := &Config{Profile: tt.profile}
